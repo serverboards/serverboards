6c6c8c8d48cbb82a2f03f7fb778e1efb6f227d8e
David Moreno <dmoreno@coralbits.com>
2016-03-14 10:53:25 +0100

    Fix use of new router at io_tcp.

--- backend/apps/io_tcp/lib/io_tcp.ex
+++ backend/apps/io_tcp/lib/io_tcp.ex
@@ -3,13 +3,12 @@ require JSON
 
 defmodule Serverboards.IoTcp do
 	use Application
-	alias Serverboards.IoTcp, as: IoTcp
-	alias Serverboards.Router, as: Router
+	alias Serverboards.{IoTcp, Router, Peer}
 
 	def start(_type, _args) do
 		import Supervisor.Spec
 
-		router=Router
+		{:ok, router} = Router.Basic.start_link
 
 		children = [
 			worker(Task, [IoTcp, :accept, [router, 4040]])
@@ -46,8 +45,14 @@ defmodule Serverboards.IoTcp do
 		case :gen_tcp.recv(socket, 0) do
 			{:ok, line} ->
 				#Logger.debug("Got data #{line}")
-				res = Router.call_json(router, line)
-				#Logger.debug("Got answer #{line}")
+				{:ok, %{ "method" => method, "params" => params, "id" => id}} = JSON.decode(line)
+				{:ok, callable, method} = Router.lookup(router, method)
+
+				#Logger.debug("Found #{inspect {callable,method}}")
+
+				res=Peer.call(callable, method, params)
+				{:ok, res}=JSON.encode( %{ "result" => res, "id" => id})
+				#Logger.debug("Got answer #{res}, writing to #{inspect socket}")
 
 				:gen_tcp.send(socket, res <> "\n")
 
