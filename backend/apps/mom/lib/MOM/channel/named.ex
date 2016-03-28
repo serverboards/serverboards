d915d521f36c469bd108fbd61269aff2adf4ea10
David Moreno <dmoreno@coralbits.com>
2016-03-28 13:21:09 +0200

    Added named channel

---
+++ backend/apps/mom/lib/MOM/channel/named.ex
@@ -0,0 +1,32 @@
+defmodule Serverboards.MOM.Channel.Named do
+	@moduledoc ~S"""
+	Allows to have named channels
+
+		iex> alias Serverboards.MOM.Channel
+		iex> require Logger
+		iex> Channel.Named.start_link
+		iex> mch = Channel.Named.ensure_exists("my-channel")
+		iex> och = Channel.Named.ensure_exists("other-channel")
+		iex> mch == och
+		false
+		iex> Channel.subscribe(mch, &Logger.info("Test" ++ (inspect &1)))
+		0
+	"""
+
+	def start_link do
+		Agent.start_link(fn -> %{} end, name: __MODULE__)
+	end
+
+	def ensure_exists(name) do
+		Agent.get_and_update(__MODULE__, fn channels ->
+			case Map.get(channels, name) do
+				nil ->
+					{:ok, nch} = Serverboards.MOM.Channel.start_link
+					channels = Map.put(channels, name, nch)
+					{nch, channels}
+				ch ->
+					{ch, channels}
+			end
+		end)
+	end
+end
