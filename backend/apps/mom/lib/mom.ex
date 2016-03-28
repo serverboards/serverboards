b2cfcf798d1292a2c9612d69144d9f1418ce4565
David Moreno <dmoreno@coralbits.com>
2016-03-28 13:15:01 +0200

    Added main mom test

--- backend/apps/mom/lib/mom.ex
+++ backend/apps/mom/lib/mom.ex
@@ -1,2 +1,24 @@
 defmodule Serverboards.MOM do
+	@moduledoc ~S"""
+	Message Oriented Middleware
+
+	Implements basic MOM to allow Message passing between the users. It is
+	used mainly as Request-Reply (RPC).
+
+
+	Example of use:
+		iex> alias Serverboards.MOM.{Channel, Tap, Message}
+		iex> {:ok, ch} = Channel.start_link
+		iex> Tap.tap(ch,"tap test")
+		iex> {:ok, ag} = Agent.start_link(fn -> nil end ) # a singleton with value nil, will be updated
+		iex> Channel.subscribe(ch, fn _ -> Agent.update(ag, fn _ -> :updated end) end)
+		iex> Agent.get(ag, &(&1))
+		nil
+		iex> Channel.send(ch, %Message{})
+		iex> Agent.get(ag, &(&1))
+		:updated
+
+	"""
+
+
 end
--- backend/apps/mom/test/mom_test.exs
+++ backend/apps/mom/test/mom_test.exs
@@ -2,6 +2,7 @@ defmodule Serverboards.MOMTest do
   use ExUnit.Case
   doctest Serverboards.MOM
   doctest Serverboards.MOM.Channel
+  doctest Serverboards.MOM.Channel.Named
   doctest Serverboards.MOM.Tap
 
   test "the truth" do
