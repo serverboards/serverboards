5a431f13a82ca5fa2c94277b4a7bb812e7570604
David Moreno <dmoreno@coralbits.com>
2016-03-28 18:58:32 +0200

    Removed router app.

--- backend/apps/router/test/callable_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule Serverboards.CallableTest do
-  use ExUnit.Case
-  doctest Serverboards.Router.Callable
-
-  test "Functions are callable" do
-    assert Serverboards.Router.Callable.call(fn [] -> "echo" end, "", []) == "echo"
-  end
-end
