085e82bd654e42611f93f4b161a55f2506c6d9ab
David Moreno <dmoreno@coralbits.com>
2016-04-08 17:19:21 +0200

    Use ES6 classes, some login behaviour

--- frontend/js/components/login.js
+++ frontend/js/components/login.js
@@ -1,35 +1,72 @@
 import React from 'react';
 
-var Login = React.createClass({
-  render: function(){
+class Login extends React.Component{
+  constructor(props){
+    super(props)
+
+    this.state = { email: '', password: '' }
+
+    // Really?? ES6 react...
+    this.handle_submit=this.handle_submit.bind(this)
+    this.update_password=this.update_password.bind(this)
+    this.update_email=this.update_email.bind(this)
+  }
+  update_password(event){
+    this.setState({password: event.target.value})
+  }
+  update_email(event){
+    this.setState({email: event.target.value})
+  }
+  handle_submit(){
+    if ($(this.refs.el).form('validate form')){
+      console.log("Try log in")
+    }
+  }
+  componentDidMount( ){
+    self=this
+    $(this.refs.el).form({
+      on: 'blur',
+      fields: {
+        email: 'email',
+        password: 'minLength[6]'
+      }
+    })
+    $(self.refs.el).find('[type=email]').focus()
+  }
+  render(){
     return (
-      <div className="ui form">
+      <form className="ui form" ref="el">
         <div className="ui small modal active" id="login">
           <div className="header">
             Login
           </div>
+
           <div className="content">
             <div className="field">
               <label>Email</label>
-              <input type="email" id="email" placeholder="user@company.com"/>
+              <input type="email" name="email" placeholder="user@company.com"
+                onChange={this.update_email} value={this.state.email}/>
             </div>
 
             <div className="field">
               <label>Password</label>
-              <input type="password" id="password" placeholder="*******"/>
+              <input type="password" name="password" placeholder="*******"
+                onChange={this.update_password} value={this.state.password}/>
             </div>
+            <div class="ui error message"></div>
           </div>
 
           <div className="actions">
-            <button type="button" className="ui positive right labeled icon button">
+            <button type="button" className="ui positive right labeled icon button"
+              onClick={this.handle_submit}>
               Login
               <i className="caret right icon"></i>
             </button>
           </div>
         </div>
-      </div>
+      </form>
     )
   }
-})
+}
 
 export default Login;
