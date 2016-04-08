463b27c252036b820023610d7818957cfbd9694c
David Moreno <dmoreno@coralbits.com>
2016-04-08 16:18:17 +0200

    Added minimal SASS support.

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -12,7 +12,8 @@ module.exports = {
         loaders: [
             { test: /\.js?$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/ },
             { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
-            { test: /\.css$/, loader: "style!css" }
+            { test: /\.css$/, loader: "style!css" },
+            { test: /\.sass$/, exclude: /node_modules/, loader: "!style!css!sass"}
         ]
     },
     plugins: [
