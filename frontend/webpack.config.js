4473344d650507526b4bc3f059123d1dfdf99346
David Moreno <dmoreno@coralbits.com>
2016-04-12 13:07:14 +0200

    Improved use of webpack for almost everything.
    Reworked all into a app dir.

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,23 +1,44 @@
 var webpack = require('webpack');
+var path = require('path')
+
+// To Reqrite index.html with proper bundle js
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = {
     entry: [
       'webpack/hot/only-dev-server',
-      "./js/app.js"
+      "./app/js/app.js"
     ],
     output: {
-        path: __dirname + '/dist/js/',
-        filename: "bundle.js"
+        path: __dirname + '/dist/',
+        filename: "js/serverboards-[hash].js"
     },
+    devtool: "source-map",
     module: {
         loaders: [
             { test: /\.js?$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/ },
             { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
             { test: /\.css$/, loader: "style!css" },
-            { test: /\.sass$/, exclude: /node_modules/, loader: "!style!css!sass"}
+            { test: /\.sass$/, exclude: /node_modules/, loader: "!style!css?sourceMap!sass?sourceMap!../sass/serverboards.sass"},
         ]
     },
     plugins: [
-      new webpack.NoErrorsPlugin()
-    ]
-
+      new webpack.NoErrorsPlugin(),
+      HTMLWebpackPluginConfig,
+      new CopyWebpackPlugin([
+        {from:'app/css', to:'css'},
+        {from:'app/js/jquery-2.2.3.min.js', to:'js'},
+        {from:'app/js/semantic.min.js', to:'js'},
+      ])
+    ],
+    sassLoader: {
+      includePaths: [path.resolve(__dirname, "./sass")]
+    }
 };
