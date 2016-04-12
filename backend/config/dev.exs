569d9ca5e754cda123999aa20a32115ad15e302a
David Moreno <dmoreno@coralbits.com>
2016-04-12 17:59:49 +0200

    Enable watcher on dev mode for frontend.

--- backend/config/dev.exs
+++ backend/config/dev.exs
@@ -2,3 +2,9 @@ use Mix.Config
 
 require Logger
 Logger.info("Debug!")
+
+config :serverboards, Serverboards.HTTP.Endpoint,
+  debug_errors: true,
+  code_reloader: true,
+  check_origin: false,
+  watchers: [make: ["-C", "../frontend", "watch"]]
