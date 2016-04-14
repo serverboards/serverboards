599b0343ecb384f46c474b78071caa92ee2586db
David Moreno <dmoreno@coralbits.com>
2016-04-14 19:17:01 +0200

    Added a Logging middelware to redux

--- frontend/app/js/app.js
+++ frontend/app/js/app.js
@@ -2,7 +2,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 
 require("!style!css!sass!../sass/serverboards.sass")
 require('./rpc')
@@ -11,7 +11,22 @@ import Router  from './router'
 import FlashMessageList from './containers/flashmessages'
 import redux_reducers from './reducers'
 
-let store = createStore( redux_reducers )
+let store = createStore(
+   redux_reducers,
+   applyMiddleware( store => next => action => {
+     console.group(action.type)
+     let result
+     try{
+       result = next(action)
+     }
+     catch(e){
+       console.log("Error processing %o: %o", action.type, e)
+     }
+     console.log(store.getState())
+     console.groupEnd(action.type)
+     return result
+   })
+ )
 
 ReactDOM.render(
   (
--- frontend/app/js/reducers/index.js
+++ frontend/app/js/reducers/index.js
@@ -4,12 +4,12 @@ import {auth} from './auth'
 var reducers ={
   auth
 }
-
+/*
 reducers.debug=(state={}, action) => {
     console.log("REDUX %s: %o",action.type, action)
     return state
   }
-
+*/
 
 const all_reducers = combineReducers( reducers )
 
