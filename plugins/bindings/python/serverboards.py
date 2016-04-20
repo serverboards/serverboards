bd8bfca141318e32873a753340157297c1a28c3d
David Moreno <dmoreno@coralbits.com>
2016-04-20 12:58:36 +0200

    Added initial python bindings, updated htpasswd auth

---
+++ plugins/bindings/python/serverboards.py
@@ -0,0 +1,81 @@
+import json, os
+
+try:
+    input=raw_input
+except:
+    pass
+
+rpc_registry={}
+
+def rpc_method(f):
+    if type(f)==str:
+        method_name=f
+        def regf(f):
+            #print("Registry %s: %s"%(method_name, repr(f)))
+            rpc_registry[method_name]=f
+            return f
+        return regf
+    else:
+        #print("Registry %s"%(f.__name__))
+        rpc_registry[f.__name__]=f
+    return f
+
+def call_local(rpc):
+    f=rpc_registry.get(rpc['method'])
+    if f:
+        params=rpc['params']
+        if type(params)==dict:
+            res=f(**params)
+        else:
+            res=f(*params)
+        return {
+            'result' : res,
+            'id' : rpc['id']
+            }
+
+@rpc_method("dir")
+def __dir():
+    return rpc_registry.keys()
+
+def loop(debug=None):
+    if debug:
+        import os
+        pid="%d: "%os.getpid()
+        __debug=lambda x: debug.write(pid+x.strip()+'\n')
+    else:
+        __debug=lambda x: None
+    __debug("--- BEGIN ---")
+    while True:
+        l=sys.stdin.readline()
+        if not l:
+            __debug("--- EOF ---")
+            return
+        __debug(l)
+        rpc = json.loads(l)
+        res=call_local(rpc)
+        __debug(json.dumps(res))
+        print(json.dumps(res)+'\n')
+        sys.stdout.flush()
+
+
+class Config:
+    def __init__(self):
+        self.path=os.path.expanduser( os.environ.get('SERVERBOARDS_PATH','~/.local/serverboards/') )
+        Config.__ensure_path_exists(self.path)
+
+    def file(self, filename):
+        p=os.path.join(self.path, filename)
+        if not p.startswith(self.path):
+            raise Exception("Trying to escape from config directory.")
+        Config.__ensure_path_exists(os.path.dirname(p))
+        return p
+
+    @staticmethod
+    def __ensure_path_exists(path):
+        try:
+            os.makedirs(path, 0700)
+        except OSError as e:
+            if 'File exists' not in str(e):
+                raise
+
+config=Config()
--- plugins/fixed-auth/htpasswd-auth.py
+++ plugins/fixed-auth/htpasswd-auth.py
@@ -1,7 +1,12 @@
 #!/bin/python
-import serverboards, passwdfile, sys
 
-pwf=passwdfile.PasswdFile("/tmp/htishpasswd")
+# setup use of bindings
+import sys
+sys.path.append('../bindings/python/')
+
+import serverboards, passwdfile
+
+pwf=passwdfile.PasswdFile( serverboards.config.file("auth/htpasswd") )
 
 @serverboards.rpc_method
 def auth(email, password):
@@ -14,23 +19,27 @@ def set_password(email, password):
     pwf.update(email, password)
     return True
 
-## Run tests with: python -m doctest htpasswd-auth.py
+def cmd_mode(options):
+    check=True
+    import getpass
+    for user in options:
+        if user=='--set':
+            print("Entering in check mode")
+            check=False
+        else:
+            if check:
+                print("Check %s password"%user)
+                pw=getpass.getpass()
+                print( pwf.check(user, pw) )
+            else:
+                print("Set %s password"%user)
+                pw=getpass.getpass()
+                pwf.update(user, pw)
 
+
+## Run tests with: python -m doctest htpasswd-auth.py
 if __name__=='__main__':
     if len(sys.argv)>1:
-        check=False
-        options=sys.argv[1:]
-        import getpass
-        for user in options:
-            if user=='--check':
-                print("Entering in check mode")
-                check=True
-            else:
-                print(user)
-                pw=getpass.getpass()
-                if check:
-                    print( pwf.check(user, pw) )
-                else:
-                    pwf.update(user, pw)
+        cmd_mode(sys.argv[1:])
     else:
         serverboards.loop()
