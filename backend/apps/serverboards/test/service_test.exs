adb2a6649e05424c096e3a1fe24f23f2ae3bcc0e
David Moreno <dmoreno@coralbits.com>
2016-04-27 15:57:14 +0200

    Services RPC is working.

--- backend/apps/serverboards/test/service_test.exs
+++ backend/apps/serverboards/test/service_test.exs
@@ -14,13 +14,34 @@ defmodule ServiceTest do
     {:ok, service} = service_add "SBDS-TST3", %{ "name" => "serverboards" }, user
     {:ok, service} = service_update service.id, %{ "name" => "Serverboards" }, user
     {:ok, info} = service_info service.id, user
-    assert info["name"] == "Serverboards"
-    {:ok, json} = JSON.encode(info)
-    Logger.debug("Result json: #{json}")
-    assert not String.contains? json, "__"
+    assert info.name == "Serverboards"
 
     :ok = service_delete "SBDS-TST3", user
     assert {:error, :not_found} == service_info service.id, user
+  end
+
+  test "Update services tags" do
+    import Serverboards.Service
+
+    user = Serverboards.Auth.User.get_user("dmoreno@serverboards.io")
+    {:ok, _service} = service_add "SBDS-TST4", %{ "name" => "serverboards" }, user
+
+    {:ok, _service} = service_update "SBDS-TST4", %{ "tags" => ~w(tag1 tag2 tag3)}, user
+    {:ok, info } = service_info "SBDS-TST4", user
+    Logger.debug("Current service info: #{inspect info}")
+    assert Enum.member? info.tags, "tag1"
+    assert Enum.member? info.tags, "tag2"
+    assert Enum.member? info.tags, "tag3"
+    assert not (Enum.member? info.tags, "tag4")
+
+    service_update "SBDS-TST4", %{ "tags" => ~w(tag1 tag2 tag4) }, user
+    {:ok, info } = service_info "SBDS-TST4", user
+    assert Enum.member? info.tags, "tag1"
+    assert Enum.member? info.tags, "tag2"
+    assert not (Enum.member? info.tags, "tag3")
+    assert Enum.member? info.tags, "tag4"
+
+    :ok = service_delete "SBDS-TST4", user
   end
 
   test "Services as a client" do
@@ -33,11 +54,29 @@ defmodule ServiceTest do
     assert Enum.member? dir, "service.delete"
     assert Enum.member? dir, "service.info"
 
-    {:ok, json} = JSON.encode(Test.Client.debug client)
-    Logger.info("Debug information: #{json}")
+    #{:ok, json} = JSON.encode(Test.Client.debug client)
+    #Logger.info("Debug information: #{json}")
 
     {:ok, l} = Test.Client.call client, "service.list", []
-    assert (Enum.count l) == 0
+    Logger.info("Got services: #{inspect l}")
+    assert (Enum.count l) >= 0
+
+    {:ok, cl} = Test.Client.call client, "service.add", ["SBDS-TST5", %{ "name" => "Serverboards test", "tags" => ["tag1", "tag2"]}]
+    Logger.info("Created service #{inspect cl}")
+    {:ok, json} = JSON.encode(cl)
+    assert not String.contains? json, "__"
+
+    {:ok, cl} = Test.Client.call client, "service.info", ["SBDS-TST5"]
+    Logger.info("Info from service #{inspect cl}")
+    {:ok, json} = JSON.encode(cl)
+    assert not String.contains? json, "__"
+
+    {:ok, cls} = Test.Client.call client, "service.list", []
+    Logger.info("Info from service #{inspect cls}")
+    {:ok, json} = JSON.encode(cls)
+    assert not String.contains? json, "__"
+    assert Enum.any?(cls, &(&1["shortname"] == "SBDS-TST5"))
+
 
     Test.Client.stop(client)
   end
