3c216c5cfd4e8d4f9c45c4f981a1ebf14297808e
David Moreno <dmoreno@coralbits.com>
2016-05-03 16:09:25 +0200

    WIP Improved services component
    Use proper props from root component, initial try to use router.

--- frontend/app/js/components/sidebar.js
+++ frontend/app/js/components/sidebar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from '../router'
 
 require("../../sass/sidebar.sass")
 
@@ -7,36 +8,19 @@ function logo(name){
 }
 
 var SidebarServices = function(props){
-  var services=[
-    {shortname:"SBDSSBDSSBDSSBDS", name:"Serverboards"},
-    {shortname:"HMTV", name:"Healthmotiv"},
-    {shortname:"IBT", name:"Ibertabac"},
-    {shortname:"CRLBTS", name:"Coralbits"},
-    {shortname:"AISOY", name:"Aisoy"},
-    {shortname:"SBDS", name:"Serverboards"},
-    {shortname:"HMTV", name:"Healthmotiv"},
-    {shortname:"IBT", name:"Ibertabac"},
-    {shortname:"CRLBTS", name:"Coralbits"},
-    {shortname:"AISOY", name:"Aisoy"},
-    {shortname:"SBDS", name:"Serverboards"},
-    {shortname:"HMTV", name:"Healthmotiv"},
-    {shortname:"IBT", name:"Ibertabac"},
-    {shortname:"CRLBTS", name:"Coralbits"},
-    {shortname:"AISOY", name:"Aisoy"},
-  ]
 
   var service = function(service){
     return (
-      <a className="item" title={service.name}>
+      <Link className="item" title={service.name} to="/service/{{service.shortname}}/">
         <span className="logo">{logo(service.name)}</span>
         {service.shortname}
-      </a>
+      </Link>
     )
   }
 
   return (
     <div className="ui vertical labeled icon menu services">
-      {services.map(service)}
+      {props.services.map(service)}
     </div>
   )
 }
@@ -44,21 +28,44 @@ var SidebarServices = function(props){
 var SidebarSections = function(props){
   return (
     <div className="ui vertical menu sections">
-      <h3 className="ui item header">Serverboards</h3>
-      <a className="item">Overview</a>
-      <a className="item">Components</a>
-      <a className="item">Permissions</a>
-      <a className="item">Rules</a>
-      <a className="item">Logs</a>
+      <h3 className="ui item header">{props.name}</h3>
+      <Link className="item" to={`/service/${props.shortname}/overview`}>Overview</Link>
+      <Link className="item" to={`/service/${props.shortname}/components`}>Components</Link>
+      <Link className="item" to={`/service/${props.shortname}/permissions`}>Permissions</Link>
+      <Link className="item" to={`/service/${props.shortname}/rules`}>Rules</Link>
+      <Link className="item" to={`/service/${props.shortname}/logs`}>Logs</Link>
     </div>
   )
 }
 
 var Sidebar = function(props){
+  var props={
+    services: [
+      {shortname:"SBDSSBDSSBDSSBDS", name:"Serverboards"},
+      {shortname:"HMTV", name:"Healthmotiv"},
+      {shortname:"IBT", name:"Ibertabac"},
+      {shortname:"CRLBTS", name:"Coralbits"},
+      {shortname:"AISOY", name:"Aisoy"},
+      {shortname:"SBDS", name:"Serverboards"},
+      {shortname:"HMTV", name:"Healthmotiv"},
+      {shortname:"IBT", name:"Ibertabac"},
+      {shortname:"CRLBTS", name:"Coralbits"},
+      {shortname:"AISOY", name:"Aisoy"},
+      {shortname:"SBDS", name:"Serverboards"},
+      {shortname:"HMTV", name:"Healthmotiv"},
+      {shortname:"IBT", name:"Ibertabac"},
+      {shortname:"CRLBTS", name:"Coralbits"},
+      {shortname:"AISOY", name:"Aisoy"},
+    ],
+    current:"SBDS"
+  }
+
+
+  var current_section=props.services.find( (el) => (el.shortname == props.current) )
   return (
     <div className="ui left container">
-      <SidebarServices/>
-      <SidebarSections/>
+      <SidebarServices {...props}/>
+      <SidebarSections {...current_section}/>
     </div>
   )
 }
