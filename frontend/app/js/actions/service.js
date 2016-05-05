9974858db29ef36cc43e392bf4a2907f3df1f03b
David Moreno <dmoreno@coralbits.com>
2016-05-05 17:21:04 +0200

    Gather available components from the backend

--- frontend/app/js/actions/service.js
+++ frontend/app/js/actions/service.js
@@ -2,42 +2,13 @@ import rpc from '../rpc'
 import Flash from '../flash'
 
 function components_update_catalog(){
-  return {
-    type:"UPDATE_COMPONENTS",
-    components: [
-      {
-        id: "core.generic",
-        name: "Generic Server",
-        description: "Assorted data for a generic server",
-        type: "generic",
-        configuration: [
-          {name: "URL", type: "url"}
-        ]
-      },
-      {
-        id: "core.email",
-        name: "Email server",
-        description: "Email server",
-        type: "generic",
-        configuration: [
-          {name: "URL", type: "url"}
-        ]
-      },
-      {
-        id: "core.email_account",
-        name: "Email account",
-        description: "Email account data",
-        type: "email",
-        configuration: [
-          {name: "email", type: "email"},
-          {name: "smtp_server", label:"SMTP Server", type: "text"},
-          {name: "imap_server", label:"IMAP Server", type: "text"},
-          {name: "username", type: "text"},
-          {name: "password_pw", type: "password"}
-        ]
-      },
-
-    ]
+  return function(dispatch){
+    rpc.call("component.available",[]).then(function(components){
+      dispatch({
+        type:"UPDATE_COMPONENTS",
+        components: components
+      })
+    })
   }
 }
 
