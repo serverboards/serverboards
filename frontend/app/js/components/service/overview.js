bf504f61ce3f9eeca1c9e0365ccc7e0cc555c552
David Moreno <dmoreno@coralbits.com>
2016-05-06 17:28:35 +0200

    Updated service overview

--- frontend/app/js/components/service/overview.js
+++ frontend/app/js/components/service/overview.js
@@ -1,8 +1,22 @@
 import React from 'react'
 
 function Overview(props){
+  function tag(props){
+    return (
+      <span className="ui label">
+        {props}
+      </span>
+    )
+  }
+
   return (
-    <h1>Default page for {props.service.name}</h1>
+    <div className="ui background white central">
+    <div className="ui text container">
+      <h1>{props.service.name}</h1>
+      {props.service.tags.map( (t) => tag(t) )}
+      <div className="ui text segment">{props.service.description}</div>
+      </div>
+    </div>
   )
 }
 
