a39fd2222ee6c0802160690754bcb3eed25986fb
David Moreno <dmoreno@coralbits.com>
2016-05-10 15:37:57 +0200

    Loading with loader

--- frontend/app/js/components/loading.js
+++ frontend/app/js/components/loading.js
@@ -2,9 +2,13 @@ import React from 'react'
 
 function Loading(props){
   return (
-    <div className="">
-      <h1>Loading data</h1>
-      {props.children}
+    <div className="ui ui main area white background segment">
+      <div className="ui active inverted dimmer">
+        <div className="ui text loader">
+          <h1>Loading data</h1>
+          {props.children}
+        </div>
+      </div>
     </div>
   )
 }
