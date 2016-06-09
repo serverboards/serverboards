f9e33c18f291a0370f214a0657d8f03000d7bfdb
David Moreno <dmoreno@coralbits.com>
2016-06-09 13:02:09 +0200

    Use properly Serverboards.Supervisor module

--- backend/apps/serverboards/lib/serverboards.ex
+++ backend/apps/serverboards/lib/serverboards.ex
@@ -1,32 +1,16 @@
 defmodule Serverboards do
-	def start(_type, _args) do
-		import Supervisor.Spec
+  def start(_type, _args) do
+    import Supervisor.Spec
 
-		children = [
-			supervisor(Serverboards.Repo, []),
-			supervisor(Task.Supervisor, [[name: Serverboards.IO.TaskSupervisor]]),
-			worker(Task, [Serverboards.IO.TCP, :accept, [4040]]),
-			worker(Serverboards.Settings, [ [name: Serverboards.Settings] ]),
-			worker(Serverboards.IO.HTTP, [:start_link, [8080]]),
-			worker(Serverboards.Auth, [:start_link, []]),
-			worker(Serverboards.Plugin.Registry, [ [name: Serverboards.Plugin.Registry] ]),
-			worker(Serverboards.Plugin.Runner, [ [name: Serverboards.Plugin.Runner] ]),
-			worker(Serverboards.Serverboard, [ [name: Serverboards.Serverboard] ]),
-			worker(Serverboards.Service, [ [name: Serverboards.Service] ]),
-			worker(Serverboards.Event, [ [name: Serverboards.Event] ]),
-			worker(Serverboards.Action, [ [name: Serverboards.Action] ]),
-		]
 
-		opts = [strategy: :one_for_one, name: Serverboards.Supervisor]
+    # inspect for deadletters and invalid
+    MOM.Tap.tap(:deadletter, "deadletter")
+    MOM.Tap.tap(:invalid, "invalid")
 
-		# inspect for deadletters and invalid
-		MOM.Tap.tap(:deadletter, "deadletter")
-		MOM.Tap.tap(:invalid, "invalid")
+    Serverboards.Supervisor.start_link name: Serverboards.Supervisor
+  end
 
-		Supervisor.start_link(children, opts)
-	end
-
-	def config_change(changed, _new, removed) do
+  def config_change(changed, _new, removed) do
     Serverboards.HTTP.Endpoint.config_change(changed, removed)
     :ok
   end
--- backend/apps/serverboards/lib/serverboards/supervisor.ex
+++ backend/apps/serverboards/lib/serverboards/supervisor.ex
@@ -1,19 +1,32 @@
 require Logger
 
 defmodule Serverboards.Supervisor do
-	use Supervisor
+  use Supervisor
 
-	def start_link(opts) do
-		Supervisor.start_link(__MODULE__, :ok, opts)
-	end
+  def start_link(opts) do
+    Supervisor.start_link(__MODULE__, :ok, opts)
+  end
 
-	def init(:ok) do
-		Logger.info("Starting Serverboards supervisor")
+  def init(:ok) do
+    Logger.info("Starting Serverboards supervisor")
 
-		 children=[
-			 worker(Serverboards.IO.TCP, [Serverboards.IO.TCP, 4040] ),
-		 ]
+    children = [
+      supervisor(Serverboards.Repo, []),
+      supervisor(Task.Supervisor, [[name: Serverboards.IO.TaskSupervisor]]),
+      worker(Task, [Serverboards.IO.TCP, :accept, [4040]]),
+      worker(Serverboards.Settings, [ [name: Serverboards.Settings] ]),
+      worker(Serverboards.IO.HTTP, [:start_link, [8080]]),
+      worker(Serverboards.Auth, [:start_link, []]),
+      worker(Serverboards.Plugin.Registry, [ [name: Serverboards.Plugin.Registry] ]),
+      worker(Serverboards.Plugin.Runner, [ [name: Serverboards.Plugin.Runner] ]),
+      worker(Serverboards.Serverboard, [ [name: Serverboards.Serverboard] ]),
+      worker(Serverboards.Service, [ [name: Serverboards.Service] ]),
+      worker(Serverboards.Event, [ [name: Serverboards.Event] ]),
+      worker(Serverboards.Action, [ [name: Serverboards.Action] ]),
+    ]
 
-		 supervisor(children, strategy: :one_for_one)
-	end
+    opts = [strategy: :one_for_one]
+
+    supervise(children, opts)
+  end
 end
