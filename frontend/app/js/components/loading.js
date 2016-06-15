d5a4161b30596fd92f25ac0121c89c4ded4f64f3
David Moreno <dmoreno@coralbits.com>
2016-06-15 17:49:18 +0200

    Loading fulscreen.

--- frontend/app/js/components/loading.js
+++ frontend/app/js/components/loading.js
@@ -2,12 +2,10 @@ import React from 'react'
 
 function Loading(props){
   return (
-    <div className="ui ui main area white background segment">
-      <div className="ui active inverted dimmer">
-        <div className="ui text loader">
-          <h1>Loading data</h1>
-          {props.children}
-        </div>
+    <div className="ui active inverted dimmer">
+      <div className="ui text loader">
+        <h1>Loading data</h1>
+        {props.children}
       </div>
     </div>
   )
