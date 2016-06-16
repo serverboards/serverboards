ef171c651c65d69c91f0c3184708aa77a9ec037a
David Moreno <dmoreno@coralbits.com>
2016-06-16 12:34:56 +0200

    Fixes to be able to really send emails, fixed conf.

--- plugins/core-notifications/serverboards-email.py
+++ plugins/core-notifications/serverboards-email.py
@@ -1,33 +1,56 @@
+#!/usr/bin/python
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__),'../bindings/python/'))
 import serverboards
 
 import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+from email.MIMEBase import MIMEBase
 
-config={}
+settings={
+    "servername" : "mail.serverboards.io",
+    "port" : "",
+    "from" : "test@serverboards.io",
+    "username" : "",
+    "password_pw" : ""
+}
 
 def render_message(_to, user, message):
     return message["body"]
 
 @serverboards.rpc_method
 def send_email(user=None, config=None, message=None):
-    _to = config.get("email") or user["email"]
+    _to = config and config.get("email") or user["email"]
     msg = MIMEMultipart('alternative')
 
     body = message["body"]
     body_html = render_message(_to=_to, user=user, message=message)
-    msg.attach(MIMEText(body_html,"plain",'UTF-8'))
-    msg.attach(MIMEText(body,"html",'UTF-8'))
+    msg.attach(MIMEText(body_html,"html",'UTF-8'))
+    msg.attach(MIMEText(body,"plain",'UTF-8'))
 
-    msg["From"]=config["from"]
+    msg["From"]=settings["from"]
     msg["To"]=_to
     msg["Subject"]=message["subject"]
 
-    smtp = smtplib(config["servername"], int(config["port"] or "0"))
-    if config["username"]:
-        smtp.login(config["username"], config["password_pw"])
-    smtp.sendmail(config["from"], _to, msg)
+    smtp = smtplib.SMTP(settings["servername"], int(settings["port"] or "0"))
+    if settings.get("username") :
+        smtp.login(settings["username"], settings["password_pw"])
+    smtp.sendmail(settings["from"], _to, msg.as_string())
     smtp.close()
 
+    return True
 
-serverboards.loop()
+
+
+if len(sys.argv)==2 and sys.argv[1]=="test":
+    print send_email(
+        user={ "email": "dmoreno@serverboards.io" },
+        config={},
+        message={
+            "subject":"This is a test message",
+            "body":"Boy of the test message",
+            "extra":[]
+        })
+else:
+    serverboards.loop()
