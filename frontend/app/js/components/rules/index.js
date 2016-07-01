7794a6086c74e461a359d959abd09639bc1de4a0
David Moreno <dmoreno@coralbits.com>
2016-07-01 19:16:39 +0200

    Show rules as cards

--- frontend/app/js/components/rules/index.js
+++ frontend/app/js/components/rules/index.js
@@ -2,6 +2,9 @@ import React from 'react'
 import RuleEdit from 'app/containers/rules/edit'
 import Loading from 'app/components/loading'
 
+const favicon = require("../../../imgs/square-favicon.svg")
+require('sass/rules.sass')
+
 function find_by_id(id, catalog){
   if (!catalog)
     return undefined
@@ -23,21 +26,39 @@ function Rule(props){
   const rule = props.rule
 
   return (
-    <tr onClick={props.onOpenEdit} title={rule.description} style={{cursor: "pointer"}} className={rule.is_active ? "positive" : ""} >
-      <td>{rule.name}</td>
-      <td>{(find_by_uuid(rule.service, props.service_catalog) || {name: rule.service}).name}</td>
-      <td>{(find_by_id(rule.trigger.trigger, props.trigger_catalog) || {nmme: rule.trigger.trigger }).name}</td>
-      <td>{Object.keys(rule.actions).map((state) => {
+    <div className="rule card">
+      <div className="header content">
+        <img src={favicon} className="right floated mini image"/>
+        <h2 clasName="ui header">{rule.name}</h2>
+        <div className="meta">{rule.description}</div>
+        <div>{(find_by_uuid(rule.service, props.service_catalog) || {name: rule.service}).name}</div>
+      </div>
+      <div className="content">
+        <h3 className="ui header uppercase">When</h3>
+        <div className="meta">
+          {(find_by_id(rule.trigger.trigger, props.trigger_catalog) || {nmme: rule.trigger.trigger }).name}
+        </div>
+      </div>
+      <div className="content">
+        <h3 className="ui header uppercase">On</h3>
+        <div>{Object.keys(rule.actions).map((state) => {
         const ac=rule.actions[state]
         return (
           <div><b>{state}:</b>
-            <br/><span style={{paddingLeft: 10}}>
+            <span style={{paddingLeft: 10}} className="meta">
             {(find_by_id(ac.action, props.action_catalog) || {name: ac.action}).name}
           </span></div>
         )
-      })}</td>
-      <td><i className="ui angle right icon"/></td>
-    </tr>
+      })}</div>
+      </div>
+      <div className="extra content" style={{padding:0}}>
+        <div className={`ui inverted ${ rule.is_active ? 'yellow' : 'grey'} menu bottom attached`}>
+          <a className="ui right item" style={{marginRight: 10}}  onClick={props.onOpenEdit} >
+          Edit rule <i className="ui angle right icon"/>
+          </a>
+        </div>
+      </div>
+    </div>
   )
 }
 
@@ -82,27 +103,14 @@ const Rules=React.createClass({
     }
     return (
       <div className="ui text container">
-        <h1 className="ui header">Rules for {props.serverboard.name}</h1>
-
-        <table className="ui very basic selectable table">
-          <thead>
-            <tr>
-              <th>Rule</th>
-              <th>Service</th>
-              <th>Trigger</th>
-              <th>Action</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
+        <div className="ui cards">
           {props.rules.map((r) =>
             <Rule rule={r} onOpenEdit={() => props.onOpenEdit(r)}
               trigger_catalog={props.trigger_catalog}
               service_catalog={props.service_catalog}
               action_catalog={props.action_catalog}/>
           )}
-          </tbody>
-        </table>
+        </div>
         <a href={`#/serverboard/${props.serverboard.shortname}/rules/add`} className="ui massive button add icon floating yellow">
           <i className="add icon"></i>
         </a>
