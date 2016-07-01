ece23088901a7cd3e492805e55a1c10ce40f83bf
David Moreno <dmoreno@coralbits.com>
2016-07-01 16:02:43 +0200

    New style modals.

--- frontend/app/js/components/modal.js
+++ frontend/app/js/components/modal.js
@@ -3,11 +3,18 @@ import React from 'react'
 require('sass/modal.sass')
 
 function Modal(props){
+  const logo=require("../../imgs/logo.svg")
   return (
     <div className="ui modal background">
-      <div className="ui modal active">
-      <i className="close icon" onClick={props.onClose}></i>
-      {props.children}
+      <div className="ui top menu">
+        <a href="#/">
+          <img className="logo" src={logo}/>
+        </a>
+
+        <a className="right aligned" onClick={props.onClose} title="Close popup"><i className="big close icon "/></a>
+      </div>
+      <div className="content" >
+        {props.children}
       </div>
     </div>
   )
