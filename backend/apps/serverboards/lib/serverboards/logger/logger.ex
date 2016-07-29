b09d347cf58a44aa0bae450c02ea0010c763a73b
David Moreno <dmoreno@coralbits.com>
2016-07-29 13:23:16 +0200

    Improved logger uses a backend server to keep a queue
    It forces flush after 1 second or 10 messages in queue.

--- backend/apps/serverboards/lib/serverboards/logger/logger.ex
+++ backend/apps/serverboards/lib/serverboards/logger/logger.ex
@@ -1,3 +1,6 @@
+alias Serverboards.Logger.Model
+alias Serverboards.Repo
+
 defmodule Serverboards.Logger do
   @moduledoc ~S"""
   Stores log messages into the database.
@@ -10,9 +13,6 @@ defmodule Serverboards.Logger do
     * First message always fails because database is not ready yet.
   """
   use GenEvent
-
-  alias Serverboards.Logger.Model
-  alias Serverboards.Repo
 
   @doc ~S"""
   Returns a list of mesages, it can receive a filter
@@ -43,6 +43,13 @@ defmodule Serverboards.Logger do
     {:ok, %{ lines: lines, count: count }}
   end
 
+  defp should_log?(metadata, level) do
+    cond do
+      level == :debug -> false
+      metadata[:application] == :ecto -> false
+      true -> true
+    end
+  end
 
   # implementation of Logger.log
 
@@ -51,7 +58,49 @@ defmodule Serverboards.Logger do
   end
 
   def init(_opts) do
-    {:ok, %{ ignore_applications: ignore_applications }}
+    {:ok, server} = Serverboards.Logger.Server.start_link name: Serverboards.Logger.Server
+    {:ok, %{ ignore_applications: ignore_applications, queue: [], server: server }}
+  end
+
+  def handle_event(:flush, state) do
+    Serverboards.Logger.Server.flush(state.server)
+    {:ok, state}
+  end
+  # ignore if im not group leader
+  def handle_event({_level, gl, {Logger, _, _, _}}, state)
+      when node(gl) != node() do
+    {:ok, state}
+  end
+  def handle_event({level, _group_leader, {Logger, message, timestamp, metadata}}, state) do
+    if should_log?(metadata, level) do
+      Serverboards.Logger.Server.log(state.server, {message, timestamp, metadata, level})
+    end
+    {:ok, state}
+  end
+  def handle_event({:configure, opts}, _state) do
+    IO.puts("Logs configure: #{inspect opts}")
+  end
+end
+
+defmodule Serverboards.Logger.Server do
+  @moduledoc ~S"""
+  Real logger to database; it has a queue and ensures data is properly saved
+  without delaying the logger calls.
+  """
+  use GenServer
+
+  @max_queue_size 10
+
+  def start_link(options \\ []) do
+    GenServer.start_link __MODULE__, [], options
+  end
+  def log(lg, msg) do
+    GenServer.call(lg, {:log, msg})
+    :ok
+  end
+  def flush(lg) do
+    GenServer.call(lg, :flush)
+    :ok
   end
 
   def to_json_type(v) do
@@ -65,13 +114,35 @@ defmodule Serverboards.Logger do
     end
   end
 
-  def handle_event(:flush, state) do
-    #IO.puts("Logs Flush")
-
-    {:ok, state}
+  # server impl
+  def init([]) do
+    {:ok, %{ count: 0, queue: [], timer: nil}}
   end
-  def handle_event({level, _group_leader, {Logger, message, timestamp, metadata}}, state) do
-    if (not metadata[:application] in state.ignore_applications) or (level != :debug) do
+
+  def handle_call({:log, msg}, from, state) do
+    state = %{ state | count: state.count+1, queue: [msg | state.queue] }
+    state = if state.count >= @max_queue_size do
+      {:noreply, _, state} = handle_call(:flush, from, state)
+      state
+    else
+      state
+    end
+
+    if state.timer do
+      :timer.cancel(state.timer)
+    end
+    {:ok, timer} = :timer.apply_after(1000, __MODULE__, :flush, [__MODULE__])
+    state = %{ state | timer: timer }
+
+    {:reply, :ok, state}
+  end
+
+  def handle_call(:flush, _from, state) do
+    if state.timer do
+      :timer.cancel(state.timer)
+    end
+
+    for {message, timestamp, metadata, level} <- state.queue do
       changelog = Model.Line.changelog(%Model.Line{}, %{
         message: to_string(message),
         level: to_string(level),
@@ -88,15 +159,9 @@ defmodule Serverboards.Logger do
       catch
         :exit, reason ->
           IO.puts("Error storing log line: #{inspect reason} (maybe not ready yet)")
-          :error
       end
     end
 
-    #IO.puts("Logs #{inspect level} #{inspect group_leader} #{inspect message} #{inspect timestamp} #{inspect metadata}")
-
-    {:ok, state}
-  end
-  def handle_event({:configure, opts}, _state) do
-    IO.puts("Logs configure: #{inspect opts}")
+    {:reply, state.count, %{ count: 0, queue: [], timer: nil}}
   end
 end
