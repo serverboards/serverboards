7a00711a51bac1bfab5fde846ab5ccdb2dabcc6d
David Moreno <dmoreno@coralbits.com>
2016-08-16 14:03:49 +0200

    Added trigger for socket is up

--- plugins/core-triggers/serverboards-triggers.py
+++ plugins/core-triggers/serverboards-triggers.py
@@ -3,6 +3,7 @@
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__),'../bindings/python/'))
 import serverboards, time, subprocess, re, requests
+import socket, urlparse
 
 td_to_s_multiplier=[
     ("ms", 0.001),
@@ -13,6 +14,8 @@ td_to_s_multiplier=[
 ]
 
 def time_description_to_seconds(td):
+    if type(td) in (int, float):
+        return float(td)
     for sufix, multiplier in td_to_s_multiplier:
         if td.endswith(sufix):
             return float(td[:-len(sufix)])*multiplier
@@ -34,31 +37,61 @@ def real_http(url=None):
         serverboards.rpc.log("Http get %.2f s"%ret.elapsed.total_seconds())
         return ret.elapsed.total_seconds()
     except:
-        return 99999 # ~7 days.. to mark timeout for sure
+        return False
+
+def real_socket_up(uri=None):
+    purl=urlparse.urlparse(uri)
+    host=purl.hostname
+    port=purl.port or str(purl.scheme)
+    #serverboards.rpc.debug("<%s> <%s> %s"%(host, type(port), repr(purl)))
+
+    initt=time.time()
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(5)
+    try:
+        for address in socket.getaddrinfo(host, port):
+            #serverboards.rpc.debug("<%s>"%(repr(address)))
+            try:
+                result = sock.connect_ex( address[4] )
+                if result == 0:
+                    return time.time() - initt
+            except (IOError, TypeError):
+                pass # normally no IPv6 support
+        return False
+    except:
+        import traceback
+        traceback.print_exc()
+        return False
 
 @serverboards.rpc_method
 def ping(ip=None, frequency=30, grace=60):
     serverboards.rpc.reply("ok")
 
-    frequency=float(frequency)
-    grace=float(grace)
     response_limit(lambda: real_ping(ip), "ping", frequency, grace)
 
 @serverboards.rpc_method
 def http(url=None, maxs=1, frequency=30, grace=60):
     serverboards.rpc.reply("ok")
 
-    frequency=float(frequency)
-    grace=float(grace)
     maxs=float(maxs)
     response_limit(lambda: real_http(url) <= maxs, "http", frequency, grace)
 
+@serverboards.rpc_method
+def socket_is_up(uri=None, frequency=30, grace=60):
+    serverboards.rpc.reply("ok")
+
+    response_limit(lambda: real_socket_up(uri), "socket_is_up", frequency, grace)
+
 def response_limit(check, type, frequency, grace):
+    frequency=time_description_to_seconds(frequency)
+    grace=time_description_to_seconds(grace)
     mgrace=grace
     is_up=True
     while True:
+        check_result=check()
+        serverboards.rpc.debug("Check result: %s"%check_result)
         if is_up:
-            if not check():
+            if not check_result:
                 mgrace-=frequency
             else:
                 mgrace=grace
@@ -67,7 +100,7 @@ def response_limit(check, type, frequency, grace):
                 serverboards.rpc.event("trigger", {"type": type, "state" : "down"})
                 is_up=False
         else:
-            if check():
+            if check_result:
                 serverboards.rpc.event("trigger", {"type": type, "state" : "up"})
                 is_up=True
                 mgrace=grace
@@ -82,4 +115,4 @@ def periodic(timeout=None):
         time.sleep(s)
         serverboards.rpc.event("trigger", {"type": "periodic", "state" : "tick"})
 
-serverboards.loop()
+serverboards.loop(debug=True)
