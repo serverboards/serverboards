03a3182fcb6fd8b869597722bf7b5a54cfe19a07
David Moreno <dmoreno@coralbits.com>
2016-10-17 14:02:31 +0200

    Moved backup monitor to its own repository

--- plugins/backup-monitor/rollup.manager.config.js
+++
@@ -1,9 +0,0 @@
-import babel from 'rollup-plugin-babel';
-
-export default {
-  entry: 'src/manager.js',
-  format: 'cjs',
-  plugins: [ babel() ],
-  dest: 'static/manager.js',
-  sourceMap: true
-};
