1c5ba450022717cfb612301e745cb5edcf9b1c71
David Moreno <dmoreno@coralbits.com>
2016-10-19 12:17:24 +0200

    Fixed action reducer may remove catalog on PS
    Also use the utils.merge function

--- frontend/app/js/reducers/action.js
+++ frontend/app/js/reducers/action.js
@@ -1,3 +1,5 @@
+import {merge} from 'app/utils'
+
 // Default status, put over current
 const default_state={
   actions: [],
@@ -7,18 +9,18 @@ const default_state={
 export function action(state=default_state, action){
   switch(action.type){
     case "ACTION_PS":
-      return { actions: action.actions }
+      return merge(state, { actions: action.actions })
       break;
     case "@RPC_EVENT/action.started":
-      return $.extend({}, state, { actions: state.actions.concat({
+      return merge(state, { actions: state.actions.concat({
         id: action.id, uuid: action.uuid, name: action.name
       }) })
       break;
     case "@RPC_EVENT/action.stopped":
-      return $.extend({}, state, { actions: state.actions.filter( (a) => a.uuid != action.uuid ) })
+      return merge(state, { actions: state.actions.filter( (a) => a.uuid != action.uuid ) })
       break;
     case "ACTION_CATALOG":
-      return $.extend({}, state, {catalog: action.catalog})
+      return merge(state, {catalog: action.catalog})
   }
   return state
 }
