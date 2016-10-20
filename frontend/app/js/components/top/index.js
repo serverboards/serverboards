3c73a8d135f77bb0d5716234f0b88cdc308c3338
David Moreno <dmoreno@coralbits.com>
2016-10-20 13:28:16 +0200

    Fixed show process menu

--- frontend/app/js/components/top/index.js
+++ frontend/app/js/components/top/index.js
@@ -54,7 +54,7 @@ const Top = React.createClass({
     }
     if (menu)
       menu=(
-        <div onClick={props.closeMenu} style={{position: "fixed", top: 0, left: 0, width: "100%", height: "100%", background: "none"}}>
+        <div onClick={props.closeMenu} style={{position: "fixed", top: 0, left: 0, width: "100vw", height: "100vh", background: "none"}}>
           {menu}
         </div>
       )
--- frontend/app/js/components/top/processesmenu.js
+++ frontend/app/js/components/top/processesmenu.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import {Link} from 'app/router'
+import {is_empty} from 'app/utils'
 
 function ProcessesMenu(props){
   return (
-    <div className="ui dropdown">
-      <div className="vertical menu" style={{position: "fixed", right: 80, top: 45}}>
-        {props.running.map((p) =>
-          <a href={`#/process/${p.uuid}`} className="item">
-            {p.name}
-          </a>
-        )}
-        <div className="ui divider"/>
+    <div className="ui dropdown pointing">
+      <div className="menu transition visible" style={{position: "fixed", left: "calc( 100vw - 240px", width: 200, top: 45}}>
+        {!is_empty(props.running) ? (
+          <div>
+            {props.running.map((p) =>
+              <a href={`#/process/${p.uuid}`} className="item">
+                {p.name}
+              </a>
+            )}
+            <div className="ui divider"/>
+          </div>
+        ) : null}
         <a href="#/process/history" className="item">
           View process history
         </a>
