435a8d57575a2bafdd0d362a7d060f55a36bee75
David Moreno <dmoreno@coralbits.com>
2016-10-21 12:51:05 +0200

    Added a generic process register
    Maps a uuid (or any key) to a pid. If the pid exits, it is removed.

---
+++ backend/apps/serverboards/test/process_registry_test.exs
@@ -0,0 +1,32 @@
+require Logger
+
+defmodule Serverboards.ProcessRegistryTest do
+  use ExUnit.Case
+  @moduletag :capture_log
+
+  :ok = Application.ensure_started(:serverboards)
+
+  test "Start, register, deregister" do
+    {:ok, registry} = Serverboards.ProcessRegistry.start_link
+
+    assert :ok == Serverboards.ProcessRegistry.add(registry, 1, self)
+    assert Serverboards.ProcessRegistry.get(registry, 1) == self()
+
+    assert Serverboards.ProcessRegistry.pop(registry, 1) == self()
+
+    Serverboards.ProcessRegistry.stop(registry)
+  end
+
+  test "Start, kill, check" do
+    {:ok, registry} = Serverboards.ProcessRegistry.start_link
+    {:ok, agent} = Agent.start_link(fn -> {} end)
+
+    :ok = Serverboards.ProcessRegistry.add(registry, 1, agent)
+    assert Serverboards.ProcessRegistry.get(registry, 1) == agent
+    Agent.stop(agent)
+    :timer.sleep(300)
+
+    assert Serverboards.ProcessRegistry.get(registry, 1) == nil
+  end
+
+end
