2507c56676882e3bfaf98987858a7ce2b4f8a921
David Moreno <dmoreno@coralbits.com>
2016-10-21 19:02:51 +0200

    Can view messages if more than visible. Limit to 5.
    Semantic UI was not allowing the dropdown bigger than the viewport.

--- frontend/app/js/components/top/notificationsmenu.js
+++ frontend/app/js/components/top/notificationsmenu.js
@@ -2,12 +2,18 @@ import React from 'react'
 import NotificationItem from 'app/components/notifications/item'
 
 function NotificationsMenu(props){
+  const unread=props.unread || []
   return (
     <div className="ui popup" id="notifications_menu">
       <div className="vertical menu">
-        {(props.unread || []).map((p) => (
+        {(unread).slice(0,5).map((p) => (
           <NotificationItem key={p.id} notification={p} now={props.open_time}/>
         ))}
+        {unread.length > 5 ? (
+          <div className="item meta disabled">
+          And {unread.length - 5} more...
+          </div>
+        ) : null}
         <a href="#/notifications/list" className="item" style={{flexDirection: "row"}}>
           View all <i className="ui chevron right icon" style={{paddingLeft: 10}}/>
         </a>
