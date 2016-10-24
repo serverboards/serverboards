c99cc05bae4aacd618fc36dca82f3c2a39252d8c
David Moreno <dmoreno@coralbits.com>
2016-10-24 13:11:12 +0200

    Added custom time picker to daterange
    Small style fixes, and now use the mimimum from the react components
    calendar

--- frontend/app/js/components/serverboard/board/daterange.js
+++ frontend/app/js/components/serverboard/board/daterange.js
@@ -6,11 +6,65 @@ import {pretty_ago} from 'app/utils'
 require("sass/calendar.sass")
 const DATE_FORMAT="YYYY-MM-DD hh:mm"
 
+const TimePicker=React.createClass({
+  propTypes:{
+    value: React.PropTypes.object.isRequired
+  },
+  componentDidMount(){
+    let self=this
+    $(this.refs.h).dropdown({
+      onChange: self.handleChangeHour
+    })
+    $(this.refs.m).dropdown({
+      onChange: self.handleChangeMinute
+    })
+  },
+  handleChangeMinute(v){
+    let val=this.props.value.minutes(v)
+    this.props.onSelect(val)
+  },
+  handleChangeHour(v){
+    let val=this.props.value.hours(v)
+    this.props.onSelect(val)
+  },
+  range(max){
+    var hh=[]
+    for (var i=0;i<max;i++){
+      hh.push( (""+"0"+i).slice(-2) )
+    }
+    return hh
+  },
+  render(){
+    const props=this.props
+    const value=props.value
+    return (
+      <div className="ui form time">
+        <select ref="h" className="ui dropdown" defaultValue={value.format("H")}>
+          {this.range(24).map( (h) =>
+            <option key={h} value={h}>{h}</option>
+          )}
+        </select>
+        <select ref="m" className="ui dropdown" defaultValue={value.format("m")}>
+          {this.range(60).map( (m) =>
+            <option key={m} value={m}>{m}</option>
+          )}
+        </select>
+      </div>
+    )
+  }
+})
+
 
 const DatetimePicker=React.createClass({
+  propTypes:{
+    value: React.PropTypes.object.isRequired,
+    onSelect: React.PropTypes.func.isRequired,
+    onClose: React.PropTypes.func.isRequired,
+  },
   getInitialState(){
     return ({
-      now: moment()
+      now: moment(),
+      value: this.props.value
     })
   },
   componentDidMount(){
@@ -27,20 +81,49 @@ const DatetimePicker=React.createClass({
     if (ev.target == this.refs.background)
       this.props.onClose()
   },
-  handleDateSelect(date){
-    this.props.onSelect(date)
+  handleDateSelect(value){
+    this.setState({value})
+  },
+  handleOk(){
+    this.props.onSelect(this.state.value)
     this.props.onClose()
   },
+  setToday(){
+    this.props.onSelect(moment())
+    this.props.onClose()
+  },
+  timePicker: (
+    <TimePicker />
+  ),
   render(){
     const props=this.props
     return (
       <span ref="el">
         <div ref="background" className="ui menu full background" onClick={this.handleClickBackground}>
           <div ref="calendar" className="ui calendar">
+            <label>Date:</label>
             <Calendar
+              value={this.state.value}
               onSelect={this.handleDateSelect}
+              onChange={this.handleDateSelect}
               disabledDate={this.isDateDisabled}
+              showToday={false}
               />
+            <label>Time:</label>
+            <TimePicker
+              value={this.state.value}
+              onSelect={this.handleDateSelect}
+              />
+            <div className="ui right" style={{marginTop: 10}}>
+              <button
+                className="ui button"
+                onClick={this.setToday}
+                >Set now</button>
+              <button
+                className="ui button yellow"
+                onClick={this.handleOk}
+                >Set selected</button>
+            </div>
           </div>
         </div>
       </span>
@@ -68,16 +151,17 @@ const DatetimeItem=React.createClass({
     const pretty=pretty_ago(props.value, props.now, 60 * 1000)
 
     return (
-      <div>
+      <div title={props.value.format("YYYY-MM-DD HH:mm")}>
         <a className="item" onClick={this.onToggleCalendar}>
           <label>{props.label}</label>
           <div className="value">
             {pretty}<br/>
-            <span className="meta" style={{color: "#bbb", fontWeight: "normal"}}>at {props.value.format("hh:mm")}</span>
+            <span className="meta" style={{color: "#bbb", fontWeight: "normal"}}>at {props.value.format("HH:mm")}</span>
             </div>
         </a>
         {this.state.open_calendar ? (
           <DatetimePicker
+            value={props.value}
             onClose={this.onToggleCalendar}
             onSelect={(value) => props.onSelect(moment(value))}
             />
