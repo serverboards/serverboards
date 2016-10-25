2f2d7918c50b6adcb67efd8a39e1a8401dff1f78
David Moreno <dmoreno@coralbits.com>
2016-10-25 12:16:58 +0200

    Moved rule card to its own module, and added more info
    * Added trigger and action card=true params to the card.

--- frontend/app/js/components/rules/index.js
+++ frontend/app/js/components/rules/index.js
@@ -1,80 +1,10 @@
 import React from 'react'
 import RuleEdit from 'app/containers/rules/edit'
 import Loading from 'app/components/loading'
-import ImageIcon from 'app/components/imageicon'
 import Command from 'app/utils/command'
-
-const icon = require("../../../imgs/rules.svg")
+import Rule from './rule'
 
 require('sass/rules.sass')
-
-function find_by_id(id, catalog){
-  if (!catalog)
-    return undefined
-  for(let tr of catalog)
-    if (tr.id==id)
-      return tr
-  return undefined
-}
-function find_by_uuid(uuid, catalog){
-  if (!catalog)
-    return undefined
-  for(let tr of catalog)
-    if (tr.uuid==uuid)
-      return tr
-  return undefined
-}
-
-function Rule(props){
-  const rule = props.rule
-
-  return (
-    <div className="rule card">
-      <div className="extra content" style={{padding:"0 10px"}}>
-        { rule.is_active ? (
-          <span><i className="ui label circular empty green"/> ON</span>
-        ) : (
-          <span><i className="ui label circular empty red"/> OFF</span>
-        )
-        }
-      </div>
-      <div className="header content">
-        <ImageIcon src={icon} className="right floated"  name={rule.name}/>
-        <h2 className="ui header">{rule.name}</h2>
-        <div className="meta">{rule.description}</div>
-        <div>{(find_by_uuid(rule.service, props.service_catalog) || {name: rule.service}).name}</div>
-      </div>
-      <div className="content">
-        <h3 className="ui header uppercase">Trigger</h3>
-        <div className="meta">
-          {(find_by_id(rule.trigger.trigger, props.trigger_catalog) || {nmme: rule.trigger.trigger }).name}
-        </div>
-      </div>
-      <div className="content">
-        <h3 className="ui header uppercase">Action</h3>
-        <div>{Object.keys(rule.actions).map((state) => {
-        const ac=rule.actions[state]
-        return (
-          <div key={state}>
-            <span className="ui meta uppercase">IF </span>
-            <span className="">{state}</span>
-            <span className="ui meta uppercase"> THEN </span>
-            <span className="">
-            {(find_by_id(ac.action, props.action_catalog) || {name: ac.action}).name}
-          </span></div>
-        )
-      })}</div>
-      </div>
-      <div className="extra content" style={{padding:0}}>
-        <div className={`ui inverted ${ rule.is_active ? 'yellow' : 'grey'} menu bottom attached`}>
-          <a className="ui right item" style={{marginRight: 10}}  onClick={props.onOpenEdit} >
-          Edit rule <i className="ui angle right icon"/>
-          </a>
-        </div>
-      </div>
-    </div>
-  )
-}
 
 const empty_rule={
   uuid: undefined,
---
+++ frontend/app/js/components/rules/rule.js
@@ -0,0 +1,110 @@
+import React from 'react'
+import ImageIcon from 'app/components/imageicon'
+
+const icon = require("../../../imgs/rules.svg")
+
+function find_by_id(id, catalog){
+  if (!catalog)
+    return undefined
+  for(let tr of catalog)
+    if (tr.id==id)
+      return tr
+  return undefined
+}
+function find_by_uuid(uuid, catalog){
+  if (!catalog)
+    return undefined
+  for(let tr of catalog)
+    if (tr.uuid==uuid)
+      return tr
+  return undefined
+}
+
+function TriggerDetails({trigger, trigger_catalog}){
+  const template=find_by_id(trigger.trigger, trigger_catalog)
+  const params=template.start.params.filter( (p) => p.card )
+  console.log(params)
+  return (
+    <div>
+      <h5 className="ui header" style={{marginBottom: 0}}>{(template || {name: trigger.trigger }).name}</h5>
+      <div style={{paddingLeft: 10}}>
+      {params.map( (p) => (
+        <div key={p.name} className="ui oneline"><label>{p.label}:</label> <span className="meta">{trigger.params[p.name]}</span></div>
+      ))}
+      </div>
+    </div>
+  )
+}
+
+function ActionDetails({action, state, action_catalog}){
+  let action_template=find_by_id(action.action, action_catalog)
+  console.log(action_template)
+  let params
+  if (action_template)
+    params=action_template.extra.call.params.filter( (p) => p.card )
+  else {
+    params = []
+    action_template={name: action.action}
+  }
+
+  return (
+    <div>
+      <div>
+        <span className="ui meta uppercase">IF </span>
+        <span className="">{state}</span>
+        <span className="ui meta uppercase"> THEN </span>
+        <span className="">
+        {action_template.name}
+        </span>
+      </div>
+      <div style={{paddingLeft: 10}}>
+        {params.map( (p) => (
+          <div key={p.name} className="ui oneline"><label>{p.label}:</label> <span className="meta">{action.params[p.name]}</span></div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+
+function Rule(props){
+  const rule = props.rule
+
+  return (
+    <div className="rule card">
+      <div className="extra content" style={{padding:"0 10px"}}>
+        { rule.is_active ? (
+          <span><i className="ui label circular empty green"/> ON</span>
+        ) : (
+          <span><i className="ui label circular empty red"/> OFF</span>
+        )
+        }
+      </div>
+      <div className="header content">
+        <ImageIcon src={icon} className="right floated"  name={rule.name}/>
+        <h2 className="ui header">{rule.name}</h2>
+        <div className="meta">{rule.description}</div>
+        <div>{(find_by_uuid(rule.service, props.service_catalog) || {name: rule.service}).name}</div>
+      </div>
+      <div className="content">
+        <h3 className="ui header uppercase">Trigger</h3>
+        <TriggerDetails trigger={rule.trigger} trigger_catalog={props.trigger_catalog}/>
+      </div>
+      <div className="content">
+        <h3 className="ui header uppercase">Action</h3>
+        <div>{Object.keys(rule.actions).map((state) => (
+          <ActionDetails key={state} action={rule.actions[state]} state={state} action_catalog={props.action_catalog}/>
+        ))}</div>
+      </div>
+      <div className="extra content" style={{padding:0}}>
+        <div className={`ui inverted ${ rule.is_active ? 'yellow' : 'grey'} menu bottom attached`}>
+          <a className="ui right item" style={{marginRight: 10}}  onClick={props.onOpenEdit} >
+          Edit rule <i className="ui angle right icon"/>
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Rule
