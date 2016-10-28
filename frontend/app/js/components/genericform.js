6beeb7ec54ebd960d894e38c9433b37439b3f0a7
David Moreno <dmoreno@coralbits.com>
2016-10-28 16:55:43 +0200

    Splitted ServiceSelect into a new improved component.
    Properly filters by current serverboard

--- frontend/app/js/components/genericform.js
+++ frontend/app/js/components/genericform.js
@@ -50,6 +50,58 @@ const RichDescription=React.createClass({
   }
 })
 
+const SelectService=React.createClass({
+  contextTypes:{
+    store: React.PropTypes.object.isRequired
+  },
+  getInitialState(){
+    return { items: [] }
+  },
+  componentDidMount(){
+    let self=this
+    let filter = {}
+    let store_state = this.context.store.getState()
+    if (self.props.traits)
+      filter.traits=self.props.traits
+    if (store_state.serverboard.current)
+      filter.serverboard=store_state.serverboard.current
+
+    rpc.call("service.list", filter).then( (services) => {
+      const results=services.map( (s) => ({
+        //name: s.name,
+        value: s.uuid,
+        name: s.name,
+        description: s.fields.filter( (p) => p.card ).map( (p) => p.value ).join(',')
+      }))
+      self.setState({items: results})
+      $(self.refs.select).dropdown({
+        onChange(value){
+          self.props.setValue(self.props.name, value)
+        }
+      })
+    })
+  },
+  render(){
+    const props = this.props
+    return (
+      <div className="field">
+        <label>{props.label}</label>
+        <RichDescription className="ui meta" value={props.description} vars={props.vars}/>
+        <div ref="select" className={`ui fluid ${props.search ? "search" : ""} selection dropdown`}>
+          <input type="hidden" name={props.name} defaultValue={props.value} onChange={props.onChange}/>
+          <i className="dropdown icon"></i>
+          <div className="default text" style={{display:"block"}}>{props.value || props.placeholder}</div>
+          <div className="menu">
+            {(this.state.items || []).map( (ac) => (
+              <div key={ac.id} className="item" data-value={ac.value}>{ac.name}<span className="ui meta" style={{float:"right"}}>{ac.description}</span></div>
+            ))}
+          </div>
+        </div>
+      </div>
+    )
+  }
+})
+
 let GenericField=React.createClass({
   getInitialState(){
     return { items: [] }
@@ -62,24 +114,6 @@ let GenericField=React.createClass({
     switch (this.props.type){
       case 'select':
         $(this.refs.select).dropdown()
-        break;
-      case 'service':
-        let self=this
-        rpc.call("service.list", {traits: (self.props.traits || [])}).then( (services) => {
-          console.log("Got services: %o", services)
-          const results=services.map( (s) => ({
-            //name: s.name,
-            value: s.uuid,
-            name: s.name,
-            description: s.fields.filter( (p) => p.card ).map( (p) => p.value ).join(',')
-          }))
-          self.setState({items: results})
-          $(self.refs.select).dropdown({
-            onChange(value){
-              self.props.setValue(self.props.name, value)
-            }
-          })
-        })
         break;
       default:
         ;;
@@ -164,20 +198,7 @@ let GenericField=React.createClass({
         )
       case 'service':
         return (
-          <div className="field">
-            <label>{props.label}</label>
-            <RichDescription className="ui meta" value={props.description} vars={props.vars}/>
-            <div ref="select" className={`ui fluid ${props.search ? "search" : ""} selection dropdown`}>
-              <input type="hidden" name={props.name} defaultValue={props.value} onChange={this.handleChange}/>
-              <i className="dropdown icon"></i>
-              <div className="default text" style={{display:"block"}}>{props.value || props.placeholder}</div>
-              <div className="menu">
-                {(this.state.items || []).map( (ac) => (
-                  <div key={ac.id} className="item" data-value={ac.value}>{ac.name}<span className="ui meta" style={{float:"right"}}>{ac.description}</span></div>
-                ))}
-              </div>
-            </div>
-          </div>
+          <SelectService {...props} onChange={this.handleChange}/>
         )
       default:
         return (
