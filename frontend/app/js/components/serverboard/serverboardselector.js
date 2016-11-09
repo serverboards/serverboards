617f9b8ce0fa6e6396e1e20838472a4939ac2c8c
David Moreno <dmoreno@coralbits.com>
2016-11-09 13:59:14 +0100

    Serverboard selector search works

--- frontend/app/js/components/serverboard/serverboardselector.js
+++ frontend/app/js/components/serverboard/serverboardselector.js
@@ -1,29 +1,68 @@
 import React from 'react'
 import LogoIcon from '../logoicon'
 
-function Selector(props){
-  console.log(props)
-  const selected=props.current
-  return (
-    <div className="ui vertical menu serverboard selector">
-      <div className="ui search">
-        <div className="ui icon input">
-          <input className="prompt" type="text" placeholder="Search by name"/>
-          <i className="search icon"/>
+const Selector=React.createClass({
+  getInitialState(){
+    return {
+      search: undefined
+    }
+  },
+  setSearch(search){
+    this.setState({search})
+  },
+  componentDidMount(){
+    $(this.refs.search)
+      .focus()
+      .on('keypress', (ev) => {
+        console.log(ev)
+        if (ev.code==27){
+          ev.preventDefault()
+          this.props.onClose()
+        }
+      })
+  },
+  render(){
+    const props=this.props
+    const selected=props.current
+    let serverboards = props.serverboards
+    if (this.state.search){
+      const search = this.state.search.toLowerCase().split(" ")
+      serverboards = props.serverboards.filter( (s) => {
+        let valid = true
+        for (let q of search){
+          if (! (
+              s.name.toLowerCase().includes(q) ||
+              s.shortname.toLowerCase().includes(q) ||
+              (s.description || "").toLowerCase().includes(q)
+            ) )
+              valid=false
+        }
+        return valid
+      })
+    }
+    serverboards = serverboards.concat().sort( function(a,b){ return a.name.localeCompare(b.name) })
+
+    return (
+      <div className="ui vertical menu serverboard selector">
+        <div className="ui search">
+          <div className="ui icon input">
+            <input ref="search" className="prompt" type="text" placeholder="Search by name" onChange={(ev) => this.setSearch(ev.target.value)}/>
+            <i className="search icon"/>
+          </div>
+        </div>
+        <div className="menu">
+          {serverboards.map( (s) => (
+            <a className={`item ${ s.shortname == selected ? "active" : ""}`} onClick={() => {props.onServiceSelect(s.shortname); props.onClose()}}>
+              <div style={{display:"inline-block", paddingRight: 10}}>
+                <LogoIcon name={s.shortname}/>
+              </div>
+              {s.name}
+            </a>
+          ))}
         </div>
       </div>
-      <div className="menu">
-        {props.serverboards.map( (s) => (
-          <a className={`item ${ s.shortname == selected ? "active" : ""}`} onClick={() => {props.onServiceSelect(s.shortname); props.onClose()}}>
-            <div style={{display:"inline-block", paddingRight: 10}}>
-              <LogoIcon name={s.shortname}/>
-            </div>
-            {s.name}
-          </a>
-        ))}
-      </div>
-    </div>
-  )
-}
+    )
+  }
+})
 
 export default Selector
