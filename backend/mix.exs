eb992833886cd0973606b12d5e4be2bc0803a1f1
David Moreno <dmoreno@coralbits.com>
2016-11-21 18:16:48 +0100

    Version bump on backend and deb package to 0.6.0

--- backend/mix.exs
+++ backend/mix.exs
@@ -6,7 +6,7 @@ defmodule Backend.Mixfile do
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps,
-     version: "0.3.0",
+     version: "0.6.0",
      name: "Serverboards",
      homepage_url: "https://serverboards.io",
      docs: [
