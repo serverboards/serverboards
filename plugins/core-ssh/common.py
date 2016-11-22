3d4eba59c8956642aad8fb477e68e1b7a8060eeb
David Moreno <dmoreno@coralbits.com>
2016-11-22 16:32:39 +0100

    Ensure permissions on new SSH files

--- plugins/core-ssh/common.py
+++ plugins/core-ssh/common.py
@@ -10,7 +10,9 @@ def ensure_ID_RSA():
     if not os.path.exists(ID_RSA):
         serverboards.info("Generating new SSH key pair")
         os.system('ssh-keygen -f "%s" -N "" >&2'%(ID_RSA,))
+        os.chmod(ID_RSA, 0o0600)
     if not os.path.exists(CONFIG_FILE):
         serverboards.info("Creating the ssh config file")
         with open(CONFIG_FILE,"w+") as fd:
             fd.write("# Write here your custom configuration\n")
+        os.chmod(CONFIG_FILE, 0o0600)
