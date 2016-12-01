0499ec831d662cb929df2b1a9da6aeafa5be5a81
David Moreno <dmoreno@coralbits.com>
2016-12-01 10:20:50 +0100

    Version update

--- backend/mix.exs
+++ backend/mix.exs
@@ -6,7 +6,7 @@ defmodule Backend.Mixfile do
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps,
-     version: "0.6.20-g905a2",
+     version: "0.6.22-gcdfab",
      name: "Serverboards",
      homepage_url: "https://serverboards.io",
      docs: [
