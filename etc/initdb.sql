8ac8ef495159b3fb90a10ac81c4d50ab28afcdd0
David Moreno <dmoreno@coralbits.com>
2016-12-02 13:31:47 +0100

    Added initdb.sql and nginx.conf sample initial config
    Old serverboards.sh from / moved to etc

---
+++ etc/initdb.sql
@@ -0,0 +1,4 @@
+-- Please double check defaults before executing.
+CREATE DATABASE serverboards WITH ENCODING 'utf8';
+CREATE USER serverboards WITH PASSWORD 'serverboards';
+GRANT ALL ON DATABASE serverboards TO serverboards;
