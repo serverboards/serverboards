f6ed964080e309b557f35c71734b43e7db4f6278
David Moreno <dmoreno@coralbits.com>
2016-12-21 14:11:31 +0100

    Fix show specific process information

--- frontend/app/js/containers/processes/process.js
+++ frontend/app/js/containers/processes/process.js
@@ -1,16 +1,27 @@
 import event from 'app/utils/event'
-//import {update_process} from 'app/actions/processes'
+import React from 'react'
+import rpc from 'app/rpc'
+import Flash from 'app/flash'
 import ProcessView from 'app/components/processes/process'
 
-const Process = event.subscribe_connect(
-  (state) => ({
-    process: state.processes.processes && state.processes.processes[0]
-  }),
-  (dispatch, props) => ({
-
-  }),
-  (props) => [`action.stopped[${props.params.uuid}]`],
-  //(props) => [ () => update_process(props.params.uuid) ]
-)(ProcessView)
+const Process = React.createClass({
+  getInitialState(){
+    return {
+      process: undefined
+    }
+  },
+  componentDidMount(){
+    rpc.call("action.history", [this.props.params.uuid]).then( (process) => {
+      this.setState({process})
+    }).catch( (e) => {
+      Flash.error("Could not get information of the process")
+    })
+  },
+  render(){
+    return (
+      <ProcessView {...this.state} {...this.props}/>
+    )
+  }
+})
 
 export default Process
