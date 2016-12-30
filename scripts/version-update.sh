0bf3f7b7da71060403b44ac58cffa272dc8b72c1
David Moreno <dmoreno@coralbits.com>
2016-12-30 18:38:33 +0100

    If cant guess version, use previous, and do not update anything

--- scripts/version-update.sh
+++ scripts/version-update.sh
@@ -6,7 +6,7 @@ cd "$( dirname $0 )/.."
 
 function get_version(){
 	local VERSION=$( git describe --match "v[0-9]*" --tags --abbrev=5 HEAD 2>/dev/null | cut -b2- | sed "s/.0-\(.*\)-/.\\1-/g" )
-	[ "$VERSION" ] || (echo "Cant guess version."; exit 1)
+	[ "$VERSION" ] || get_prev_version
 	echo $VERSION
 }
 
