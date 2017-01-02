ab1fde5ea11d43e3b550e8762b288b36b17b678d
David Moreno <dmoreno@coralbits.com>
2017-01-02 17:57:58 +0100

    Update version.json and remote last version deb file too

--- scripts/publish-deb.sh
+++ scripts/publish-deb.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
 
+function get_version(){
+	local VERSION=$( git describe --match "v[0-9]*" --tags --abbrev=5 HEAD 2>/dev/null | cut -b2- | sed "s/.0-\(.*\)-/.\\1-/g" )
+	[ "$VERSION" ] || get_prev_version
+	echo $VERSION
+}
+
+
 rsync -avz serverboards.deb -e ssh www.serverboards.io:/downloads/
+
+VERSION=$( get_version )
+cat > latest.json << EOF
+{"version": "$VERSION"}
+EOF
+
+rsync -avz serverboards.deb -e ssh www.serverboards.io:/downloads/serverboards_${VERSION}_amd64.deb
+rsync -avz latest.json -e ssh www.serverboards.io:/downloads/
