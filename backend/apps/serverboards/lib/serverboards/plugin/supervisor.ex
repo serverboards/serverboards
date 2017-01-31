b1f76f9a4e9e445e5712240141883526678d4d56
David Moreno <dmoreno@coralbits.com>
2017-01-31 10:28:27 +0100

    Use supervisor at plugins

---
+++ backend/apps/serverboards/lib/serverboards/plugin/supervisor.ex
@@ -0,0 +1,13 @@
+defmodule Serverboards.Plugin.Supervisor do
+  def start_link(options \\ []) do
+    import Supervisor.Spec
+
+    children = [
+      worker(Serverboards.Plugin.Registry, [ [name: Serverboards.Plugin.Registry] ]),
+      worker(Serverboards.Plugin.Runner, [ [name: Serverboards.Plugin.Runner] ]),
+      worker(Serverboards.Plugin.Data, [ [name: Serverboards.Plugin.Data] ]),
+    ]
+
+    Supervisor.start_link(children, [strategy: :one_for_one] ++ options)
+  end
+end
