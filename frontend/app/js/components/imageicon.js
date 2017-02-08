de98046ec82504ef8d5504eb5fe5cccdfcf4ff1a
David Moreno <dmoreno@coralbits.com>
2017-02-08 18:22:57 +0100

    Fix blocking at ImageIcon loading
    SVG manipulation blocks the browser. Now jsu tload the SVG and super
    impose the name.
    
    Fixes #18.

--- frontend/app/js/components/imageicon.js
+++ frontend/app/js/components/imageicon.js
@@ -13,26 +13,13 @@ function logo(name){
 
 let count=0
 
-const ImageIcon = React.createClass({
-  updateText(){
-    var svg=this.refs.img.children[0]
-    var svgDoc=svg.contentDocument;
-    var text=svgDoc.getElementById("modifyme")
-    text.textContent=logo(this.props.name)
-  },
-  componentDidMount(){
-    var svg=this.refs.img.children[0]
-    svg.onload=() => { this.updateText() }
-  },
-  render(){
-    const props=this.props
-
-    return (
-      <div ref="img" className={`ui imageicon image ${props.className || "tiny"}`}>
-        <object data={props.src} type="image/svg+xml"/>
-      </div>
-    )
-  }
-})
+function ImageIcon(props){
+  return (
+    <div className={`ui imageicon image ${props.className || "tiny"}`}>
+      <img src={props.src}/>
+      <span>{logo(props.name)}</span>
+    </div>
+  )
+}
 
 export default ImageIcon
