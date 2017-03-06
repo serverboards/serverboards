0b2bbfd72fe0585867668a8f0ccf197cdedb9a09
David Moreno <dmoreno@coralbits.com>
2017-03-06 14:09:33 +0100

    Fixed webpack test babel configuration
    Required loaders and alias resolvers

--- frontend/webpack.config-test.babel.js
+++ frontend/webpack.config-test.babel.js
@@ -6,16 +6,25 @@ export default {
   externals: [nodeExternals()],
   module: {
     loaders: [
-      {
-        test: /\.js$/,
-        loader: "babel-loader"
-      }
+        //{ test: /\.jsx$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/ },
+        { test: /\.js$/, exclude: /node_modules/, loaders: ["react-hot", "babel"] },
+        { test: /\.css$/, loader: "style!css" },
+        { test: /\.sass$/, exclude: /node_modules/, loader: "style!css!sass"},
+        { test: /\.(jpe?g|png|gif|svg)$/i,
+                loaders: [
+                    'file?hash=sha512&digest=hex&name=[hash].[ext]',
+                    'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+                ]
+              },
+        { test: /\.json$/, loader: "json" },
     ]
   },
   devtool: "cheap-module-source-map",
   resolve: {
     alias:{
       app : path.resolve("./app/js"),
+      sass : path.resolve("./app/sass"),
+      lang : path.resolve("./lang")
     }
   },
 };
