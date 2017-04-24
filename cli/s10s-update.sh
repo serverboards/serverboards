abcc05fa9d7db838838ca47b1b4072a79eb095b2
David Moreno <dmoreno@coralbits.com>
2017-04-24 14:02:00 +0200

    Added s10s update command

---
+++ cli/s10s-update.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+if [ "$1" = "--one-line-help" ]; then
+  echo "Updates serverboards"
+  exit 0
+fi
+
+if [ "$(whoami)" != "root" ]; then
+  echo "Serverboards update requires root"
+  exit 1
+fi
+
+if [ -e "/etc/apt/sources.list.d/serverboards.list" ]; then
+  # Easy apt update
+  apt-get update
+  apt-get install serverboards -o Dpkg::Options::="--force-confold"
+else
+  dpkg --configure -a
+  apt-get -fy install
+  DURL=https://serverboards.io/downloads/
+  TMPDIR=$( mktemp -d )
+  cd $TMPDIR
+  VERSION=$( curl $DURL/latest.json | python3 -c "import sys, json; print(json.load(sys.stdin)['version'])" )
+  curl -o serverboards.deb $DURL/serverboards_${VERSION}_amd64.deb
+  apt-get update
+  dpkg --force-confold -i serverboards.deb
+  apt-get -f install
+  service serverboards restart
+  cd
+  rm -rf $TMPDIR
+fi
