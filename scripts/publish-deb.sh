39d608ec93637f147640e1b9a873a565dfbbcf94
David Moreno <dmoreno@coralbits.com>
2017-05-08 10:39:57 +0200

    Publish automatically to the unstable channel
    It publishes to the unstable channel on CI success. Publish to stable
    channel is not automatic but manual.

--- scripts/publish-deb.sh
+++ scripts/publish-deb.sh
@@ -2,36 +2,52 @@
 
 REMOTE=www.serverboards.io:/ubuntu/
 REPOPATH=/var/repositories/
+DEBFILE=${2:-serverboards.deb}
 
 set -e
 
-if [ ! -e "serverboards.deb" ]; then
-  echo "Need serverboards.deb in the current directory"
+if [ ! -e "$DEBFILE" ]; then
+  echo "Need $DEBFILE in the current directory,or second argument"
   exit 1
 fi
 
 case "$1" in
-  ""|"unstable")
-    rsync -avz serverboards.deb -e ssh www.serverboards.io:/downloads/
+  "unstable")
+    rm -f "$REPOPATH/dists/unstable/main/binary-amd64/Release.gpg"
 
-    VERSION=$( dpkg-deb --info serverboards.deb | grep Version | awk '{ print $2 }' )
-    echo "Current version is $VERSION"
+    # configured as explained at https://www.digitalocean.com/community/tutorials/how-to-use-reprepro-for-a-secure-package-repository-on-ubuntu-14-04
 
-    cat > latest.json << EOF
-{"version": "$VERSION"}
-EOF
-    rsync -avz serverboards.deb -e ssh www.serverboards.io:/downloads/serverboards_${VERSION}_amd64.deb
-    rsync -avz latest.json -e ssh www.serverboards.io:/downloads/
+    reprepro -b $REPOPATH includedeb unstable $DEBFILE
+    if [ ! -e "$REPOPATH/dists/unstable/Release.gpg" ]; then
+            echo "No GPG signed. Aborting upload"
+            exit 1
+    fi
+
+    rsync -avz -e ssh $REPOPATH/pool $REPOPATH/dists $REMOTE
     ;;
   "clean")
     rm -rf $REPOPATH/db $REPOPATH/dists $REPOPATH/pool
     ;;
   "stable")
+    # stable quick, from UI, not apt installed. DEPRECATED
+    rsync -avz $DEBFILE -e ssh www.serverboards.io:/downloads/
+
+    VERSION=$( dpkg-deb --info $DEBFILE | grep Version | awk '{ print $2 }' )
+    echo "Current version is $VERSION"
+
+    cat > latest.json << EOF
+{"version": "$VERSION"}
+EOF
+    rsync -avz $DEBFILE -e ssh www.serverboards.io:/downloads/serverboards_${VERSION}_amd64.deb
+    rsync -avz latest.json -e ssh www.serverboards.io:/downloads/
+
+
+    # Stable APT
     rm -f "$REPOPATH/dists/stable/main/binary-amd64/Release.gpg"
 
     # configured as explained at https://www.digitalocean.com/community/tutorials/how-to-use-reprepro-for-a-secure-package-repository-on-ubuntu-14-04
 
-    reprepro -b $REPOPATH includedeb stable serverboards.deb
+    reprepro -b $REPOPATH includedeb stable $DEBFILE
     if [ ! -e "$REPOPATH/dists/stable/Release.gpg" ]; then
             echo "No GPG signed. Aborting upload"
             exit 1
@@ -39,8 +55,8 @@ EOF
 
     rsync -avz -e ssh $REPOPATH/pool $REPOPATH/dists $REMOTE
     ;;
-  "help")
-    echo "$0 ''|unstable|stable|clean"
+  ""|"help")
+    echo "$0 ''|unstable|stable|clean <debfile>"
     echo
     echo " ''|unstable -- Releases to the unstable channel"
     echo " stable -- Releases to the stable channel"
