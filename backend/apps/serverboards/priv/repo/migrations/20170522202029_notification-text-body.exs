9437bcab78cada33163b20e295a4462199985b44
David Moreno <dmoreno@coralbits.com>
2017-06-26 19:07:54 +0200

    Fix down alter notification text migration

--- backend/apps/serverboards/priv/repo/migrations/20170522202029_notification-text-body.exs
+++ backend/apps/serverboards/priv/repo/migrations/20170522202029_notification-text-body.exs
@@ -1,9 +1,14 @@
 defmodule :"Elixir.Serverboards.Repo.Migrations.Notification-text-body" do
   use Ecto.Migration
 
-  def change do
+  def up do
     alter table :notifications_notification do
       modify :body, :text
+    end
+  end
+  def down do
+    alter table :notifications_notification do
+      modify :body, :varchar, max_length: 256
     end
   end
 end
