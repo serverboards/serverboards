f40e29170b84957d729e1a9bd23b2240edd1b980
David Moreno <dmoreno@coralbits.com>
2017-08-26 11:08:07 +0200

    Working webpack3. Reverted to react-router 2

--- frontend/app/js/utils/store.js
+++ frontend/app/js/utils/store.js
@@ -2,18 +2,17 @@ import redux_reducers from '../reducers'
 import thunk from 'redux-thunk';
 import rpc from '../rpc'
 import { createStore, applyMiddleware, compose } from 'redux'
+import { hashHistory } from 'react-router'
 import { routerMiddleware, push } from 'react-router-redux'
 import { merge, object_is_equal } from 'app/utils'
 import React from 'react'
 const react_redux_connect = require('react-redux').connect
 import serverboards_connect from 'app/containers/connect'
-import createHistory from 'history/createBrowserHistory'
+
 
 import AsyncPromises from 'app/containers/asyncpromises'
 import Subscribed from 'app/containers/subscribed'
 import Updaters from 'app/containers/updaters'
-
-const history = createHistory()
 
 var redux_extra=f => f
 
@@ -35,7 +34,7 @@ const promise_middleware = store => next => action => {
 let store = createStore(
   redux_reducers, {},
   compose(
-    applyMiddleware(promise_middleware, thunk, routerMiddleware(history)),
+    applyMiddleware(promise_middleware, thunk, routerMiddleware(hashHistory)),
     redux_extra
   )
 )
