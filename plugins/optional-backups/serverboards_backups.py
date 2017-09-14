93283e37c20642ab42741fd467b4b40a38b27155
David Moreno <dmoreno@coralbits.com>
2017-09-14 18:05:09 +0200

    Log information on backup progress
    Logs when starts, ends, many error cases. The extra logs allowed to find
    corner cases that were leaving it stuck.

--- plugins/optional-backups/serverboards_backups.py
+++ plugins/optional-backups/serverboards_backups.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-import sys, os, sys, uuid, datetime
+import sys, os, sys, uuid, datetime, sh
 sys.path.append(os.path.join(os.path.dirname(__file__),'../bindings/python/'))
 import serverboards
 from serverboards import print, plugin, Plugin, rpc
@@ -22,17 +22,28 @@ def get_backup_fn(component, type):
 
 @serverboards.rpc_method
 def backup_now(backup):
-    backup=serverboards.plugin.data.get(backup)
-    bk = Backup(backup)
+    job=serverboards.plugin.data.get(backup)
+    job["id"]=backup
+    bk = Backup(job)
     return bk.id
 
 @serverboards.rpc_method
 def backup_stop(backup):
     backup=serverboards.plugin.data.get(backup)
-    backup.update({"status":"aborted", "size": None})
-    rpc.call("plugin.data.update", backup["id"], backup)
-    project=backup["id"].split('-')[0]
-    serverboards.rpc.event("event.emit", "serverboards.core.backup.updated[%s]"%project, backup)
+    fifofile=backup.get("fifofile")
+    if fifofile and os.path.exists(fifofile):
+        try:
+            sh.fuser("-k", fifofile)
+        except:
+            pass
+        os.unlink(fifofile)
+
+    if backup.get("status")=="running":
+        backup.update({"status":"aborted", "size": None, "fifofile": None})
+        rpc.call("plugin.data.update", backup["id"], backup)
+        project=backup["id"].split('-')[0]
+        serverboards.rpc.event("event.emit", "serverboards.core.backup.updated[%s]"%project, backup)
+        serverboards.info("Manually stopped backup", extra={"backup": backup["id"]})
 
 @serverboards.rpc_method
 def get_backup_options(*args, **kwargs):
@@ -48,51 +59,66 @@ def datetime_now():
 
 class Backup:
     def __init__(self, job):
-        self.job=job
-        self.project=job["id"].split('-')[0]
-        self.fifofile = None
-        source = job["source"]
-        destination = job["destination"]
+        self.id=job["id"]
+        serverboards.info("Starting backup.", extra={"backup": self.id} )
+        try:
+            self.project=self.id.split('-',1)[0]
+            self.job=job
+            self.fifofile = None
+            source = job["source"]
+            destination = job["destination"]
 
-        self.id = str(uuid.uuid4())
-        self.read_source = get_backup_fn(source["component"], "read_source")
-        self.write_destination = get_backup_fn(destination["component"], "write_destination")
+            self.read_source = get_backup_fn(source["component"], "read_source")
+            self.write_destination = get_backup_fn(destination["component"], "write_destination")
 
-        self.fifofile = os.path.join(TMPDIR, str(uuid.uuid4()))
-        print("Backup %s at %s"%(job["id"], self.fifofile))
-        os.mkfifo(self.fifofile, 0o0600)
-        self.source_job = self.read_source(self.fifofile, source["config"], _async=(self.source_done, self.source_error))
-        self.destination_job = self.write_destination(self.fifofile, destination["config"], _async=(self.destination_done, self.destination_error))
+            self.fifofile = os.path.join(TMPDIR, str(uuid.uuid4()))
+            print("Backup %s at %s"%(self.id, self.fifofile), extra={"backup": self.id})
+            os.mkfifo(self.fifofile, 0o0600)
+            self.source_job = self.read_source(self.fifofile, source["config"], _async=(self.source_done, self.source_error))
+            self.destination_job = self.write_destination(self.fifofile, destination["config"], _async=(self.destination_done, self.destination_error))
 
-        self.source_size=None
-        self.destination_size=None
-        self.update_job(status="running", size=None)
+            self.source_size=None
+            self.destination_size=None
+            self.update_job(status="running", size=None, fifofile=self.fifofile)
+        except:
+            serverboards.error("Error initializing backup, check configuration.", extra={"backup": self.id} )
 
     def source_done(self, size):
+        serverboards.debug("Source backup finished: %s"%str(size), extra={"backup": self.id})
         self.source_size=size
-        if self.destination_size:
+        if self.destination_size != None:
             self.finished_backup()
-        pass
     def source_error(self, error):
-        serverboards.error("Error on source backup: %s"%str(error))
+        serverboards.error("Error on source backup: %s"%str(error), extra={"backup": self.id})
         self.source_size="error"
-        if self.destination_size:
+        if self.destination_size != None:
             self.finished_backup()
     def destination_done(self, size):
+        serverboards.debug("Destination backup finished: %s"%str(size), extra={"backup": self.id})
         self.destination_size=size
-        if self.source_size:
+        if self.source_size != None:
             self.finished_backup()
     def destination_error(self, error):
-        serverboards.error("Error on destination backup: %s"%str(error))
+        serverboards.error("Error on destination backup: %s"%str(error), extra={"backup": self.id})
         self.destination_size="error"
-        if self.source_size:
+        if self.source_size != None:
             self.finished_backup()
 
     def finished_backup(self):
-        if self.source_size == "error" or self.destination_size=="error":
-            self.update_job(status="error", size=None, completed_date=datetime_now())
+        if (self.source_size == "error" or self.destination_size == "error" or
+            self.source_size == 0 or self.destination_size == 0
+            ):
+            serverboards.error(
+                "Backup error. Read %s bytes, write %s bytes."%(self.source_size, self.destination_size),
+                extra={"backup": self.id}
+                )
+            self.update_job(status="error", size=None, completed_date=datetime_now(), fifofile=None)
         else:
-            self.update_job(status="ok", size=self.destination_size, completed_date=datetime_now())
+            serverboards.info(
+                "Backup finished. Read %s bytes, write %s bytes."%(self.source_size, self.destination_size),
+                extra={"backup": self.id}
+                )
+            self.update_job(status="ok", size=self.destination_size, completed_date=datetime_now(), fifofile=None)
 
         if self.fifofile:
             print("Unlink fifo (1)")
@@ -110,4 +136,4 @@ class Backup:
             os.unlink(self.fifofile)
 
 
-serverboards.loop(debug=True)
+serverboards.loop()
