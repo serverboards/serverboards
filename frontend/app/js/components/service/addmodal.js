cd87af5855811616afdc1192746b46ef677b0752
David Moreno <dmoreno@coralbits.com>
2017-09-21 18:40:46 +0200

    After adding a service from selector, go back to same screen
    This makes it reload all services, but forgets all status of the form,
    and do not set the new service.

--- frontend/app/js/components/service/addmodal.js
+++ frontend/app/js/components/service/addmodal.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Modal from '../modal'
 import AddService from 'app/containers/service/add'
+import store from 'app/utils/store'
 
 function AddServiceModal(props){
   return (
     <Modal className="wide">
-      <AddService {...props}/>
+      <AddService
+        {...props}
+        onServiceAdded={() => store.back() }
+        />
     </Modal>
   )
 }
