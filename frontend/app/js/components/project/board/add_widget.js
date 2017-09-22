7a73db3b9af1aba8a58b5630059a44f4152df00a
David Moreno <dmoreno@coralbits.com>
2017-09-22 18:17:46 +0200

    Can add a widget from the marketplace

--- frontend/app/js/components/project/board/add_widget.js
+++ frontend/app/js/components/project/board/add_widget.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import {Loading, GenericForm, Modal, Tip, Selector, Error} from 'app/components/export'
 import rpc from 'app/rpc'
+import Flash from 'app/flash'
 import {set_modal} from 'app/utils/store'
 import i18n from 'app/utils/i18n'
 import cache from 'app/utils/cache'
+import plugin from 'app/utils/plugin'
 
 class SetupWidget extends React.Component{
   constructor(props){
@@ -35,6 +37,108 @@ class SetupWidget extends React.Component{
     )
   }
 }
+
+
+function get_widget_market_catalog(){
+  return Promise.all([
+      plugin.start_call_stop(
+            "serverboards.optional.update/updater",
+            "component_filter",
+            {type: "widget"}
+          )
+      , cache.plugins()
+    ]).then( (cp) => {
+      const catalog = cp[0]
+      const plugin_list = cp[1]
+
+      console.log("Got catalog %o // %o", catalog, plugin_list)
+      return catalog.filter( c => !plugin_list[c.plugin] )
+    })
+}
+
+class SelectWidget extends React.Component{
+  constructor(props){
+    super(props)
+
+    this.state = {
+      tab: 1
+    }
+  }
+  handleInstallWidget(w){
+    this.setState({tab:3})
+    plugin.install(w.giturl).then(() => {
+      w = {...w, type: w.id} // I need the component id in the type field.
+      this.props.onSelectWidget(w)
+    }).catch((e) => {
+      console.error(e)
+      this.setState({tab:2})
+      Flash.error(i18n("Error installing *{plugin}*. Please try again or check logs.", {plugin:w.name}))
+    })
+  }
+  render(){
+    const {tab} = this.state
+
+    return (
+      <div className="extend">
+        <div className="ui attached top form">
+          <div className="ui input seamless white">
+            <i className="icon search"/>
+            <input
+              type="text"
+              onChange={(ev) => {
+                this.setState({filter:ev.target.value.toLocaleLowerCase().split(' ')})
+              }}
+              placeholder={i18n("Filter...")}
+              />
+          </div>
+        </div>
+        <div className="ui padding">
+          <h2 className="ui centered header">
+            <i className={`icon cloud`}/>
+            {i18n("Add a widget to this dashboard")}
+          </h2>
+          <div>
+            {i18n("Select the widget you want to add to the dashboard. If you can not find what you are looking for, check the marketplace.")}
+          </div>
+        </div>
+        <div className="ui separator" style={{height:10}}/>
+        <div className="ui pointing secondary menu">
+          <a className={`item ${tab==1 ? "active" : ""}`} onClick={() => this.setState({tab:1})}>
+            {i18n("Available services")}
+          </a>
+          <a className={`item ${tab==2 ? "active" : ""}`} onClick={() => this.setState({tab:2})}>
+            {i18n("Marketplace")}
+          </a>
+        </div>
+        {tab == 1 ? (
+          <Selector
+            key="installed"
+            get_items={cache.widget_catalog}
+            onSelect={this.props.onSelectWidget}
+            current={(this.props.widget || {}).id}
+            show_filter={false}
+            filter={this.state.filter}
+          />
+        ) : (tab==2) ? (
+          <Selector
+            key="marketplace"
+            get_items={get_widget_market_catalog}
+            onSelect={this.handleInstallWidget.bind(this)}
+            current={(this.props.widget || {}).id}
+            show_filter={false}
+            filter={this.state.filter}
+          />
+        ) : (
+          <Loading>
+            {i18n("Installing plugin")}
+          </Loading>
+        ) }
+      </div>
+    )
+  }
+}
+
+
 class AddWidget extends React.Component{
   constructor(props){
     super(props)
@@ -59,13 +163,11 @@ class AddWidget extends React.Component{
     switch(this.state.step){
       case 0:
         section = (
-          <Selector
-            title={i18n("Add a widget")}
-            description={i18n("Select the widget to add to the {name} dashboard", {name: this.props.dashboard.name})}
-            get_items={cache.widget_catalog}
-            onSelect={this.handleSelectWidget.bind(this)}
-            current={(this.state.widget || {}).id}
-          />
+          <SelectWidget
+            onSelectWidget={this.handleSelectWidget.bind(this)}
+            widget={this.state.widget}
+            {...this.props}
+            />
         )
         break;
       case 1:
