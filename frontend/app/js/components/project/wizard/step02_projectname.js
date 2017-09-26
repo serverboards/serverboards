c2df99a0e30f1a3fa393dac737c7587f35df630e
David Moreno <dmoreno@coralbits.com>
2017-09-26 18:01:43 +0200

    Can press enter to submit project name

--- frontend/app/js/components/project/wizard/step02_projectname.js
+++ frontend/app/js/components/project/wizard/step02_projectname.js
@@ -2,6 +2,12 @@ import React from 'react'
 import i18n from 'app/utils/i18n'
 import {Tip} from 'app/components'
 
+function maybe_submit(ev){
+  if (ev.charCode==13){
+    $('#project_name_submit').click()
+  }
+}
+
 export function make_projectname(props){
   return {
     tip: (
@@ -14,11 +20,12 @@ export function make_projectname(props){
       <div className="ui fill centered area">
         <h2 className="ui header">{i18n("Choose a name for your project")}</h2>
         <div className="ui form">
-          <input type="text" placeholder={i18n("Name your project")} id="project_name"/>
+          <input type="text" placeholder={i18n("Name your project")} id="project_name" onKeyPress={maybe_submit}/>
           <button
             className="ui basic teal button"
             style={{marginTop: 30}}
             onClick={() => props.handleCreateProject($('input#project_name').val())}
+            id="project_name_submit"
             >{i18n("Create and continue")}</button>
         </div>
       </div>
