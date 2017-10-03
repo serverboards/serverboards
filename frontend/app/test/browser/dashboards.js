2a0049e026d51efea690b73dbb318f94301b187d
David Moreno <dmoreno@coralbits.com>
2017-10-03 16:33:43 +0200

    Style fixes on tests

--- frontend/app/test/browser/dashboards.js
+++ frontend/app/test/browser/dashboards.js
@@ -13,27 +13,27 @@ describe("Dashboard", () => {
     $('input[name=password]').setValue("asdfasdf")
     $('button.login').click()
 
-    browser.waitUntil(() => $('body').getText().includes("This is how Serverboards works."))
+    browser.waitUntil(() => $('body').getText().includes("Start by creating your first project"))
   })
 
-  it("Creates a project", function(){
-    $('#add_project').waitForExist()
-    $('#add_project').click()
-    $('input[name=name]').waitForExist()
+  it("Creates a project -- wizard", function(){
+    $('#create_project').waitForExist()
+    $('#create_project').click()
+    $('input#project_name').waitForExist()
 
     browser.saveScreenshot("./shots/010-add-project.png")
 
 
-    $('input[name=name]').setValue("TEST-01")
-    $('textarea[name=description]').setValue("This is the TEST nr 01")
-    $('button[type=submit]').click()
-    browser.waitUntil(() => $('body').getText().includes("Start adding Widgets into your project's Dashboard"))
+    $('input#project_name').setValue("TEST-01")
+    $('#project_name_submit').click()
+    browser.waitUntil(() => $('body').getText().includes("Widgets"))
+
     $$('.message .close.icon').map( f => f.click() ) // Close messages
 
     browser.saveScreenshot("./shots/011-add-project-ok.png")
 
-    $('#project_selector').waitForExist()
-    $('#project_selector').click()
+    $('#project').waitForExist()
+    $('#project').click()
     $('=TEST-01').waitForExist()
 
     browser.saveScreenshot("./shots/017-dashboard-list-1.png")
