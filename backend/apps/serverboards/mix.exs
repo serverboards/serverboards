1c427cb95c0f32bfb6288613f602750e9ba3ad94
David Moreno <dmoreno@coralbits.com>
2017-11-17 09:19:20 +0100

    Updated version

--- backend/apps/serverboards/mix.exs
+++ backend/apps/serverboards/mix.exs
@@ -3,7 +3,7 @@ defmodule Serverboards.Mixfile do
 
   def project do
     [app: :serverboards,
-     version: "17.10.0-alpha.769",
+     version: "17.10.0-alpha.798",
      build_path: "../../_build",
      config_path: "../../config/config.exs",
      deps_path: "../../deps",
