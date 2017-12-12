dbdfe1ba3a38ddac590f893d2117838f19437c61
David Moreno <dmoreno@coralbits.com>
2017-12-12 17:52:19 +0100

    Init supervisor should be started later
    It needs the event worker and the plugins. Acually it may need to be the
    last, at the same level as rules.

--- backend/apps/serverboards/lib/serverboards/supervisor.ex
+++ backend/apps/serverboards/lib/serverboards/supervisor.ex
@@ -35,6 +35,7 @@ defmodule Serverboards.Supervisor do
       # this should be the last, as it may use others
       # worker(Serverboards.Rules, [ [name: Serverboards.Rules] ]),
       supervisor(Serverboards.RulesV2.Supervisor, [ [name: Serverboards.RulesV2.Supervisor] ]),
+      supervisor(Serverboards.Plugin.Init.Supervisor, [ [name: Serverboards.Plugin.Init.Supervisor] ]),
     ]
 
     opts = [strategy: :one_for_one]
