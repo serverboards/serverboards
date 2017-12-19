d640f7ac6014638d32127eb0ed4360b91ed6cf57
David Moreno <dmoreno@coralbits.com>
2017-12-19 17:54:24 +0100

    Fix remove broken widgets

--- frontend/app/js/components/project/board/edit_widget.js
+++ frontend/app/js/components/project/board/edit_widget.js
@@ -3,6 +3,7 @@ import Loading from 'app/components/loading'
 import GenericForm from 'app/components/genericform'
 import rpc from 'app/rpc'
 import Modal from 'app/components/modal'
+import Error from 'app/components/error'
 import HoldButton from 'app/components/holdbutton'
 import Flash from 'app/flash'
 import {set_modal} from 'app/utils/store'
@@ -29,9 +30,13 @@ const AddWidget = React.createClass({
     })
   },
   removeWidget(){
-    console.log("remove")
+    // console.log("remove", this.props.widget_id, this.props.widget.uuid)
     rpc.call("dashboard.widget.remove", [this.props.widget.uuid]).then(() => {
       set_modal(null)
+      Flash.success("Widet removed")
+    }).catch( e => {
+      console.error(e)
+      Flash.error("Could not remove widget.")
     })
   },
   setFormData(config){
@@ -43,6 +48,23 @@ const AddWidget = React.createClass({
       return (
         <Modal>
           <Loading>{i18n("Widget description")}</Loading>
+        </Modal>
+      )
+    }
+    if (widget=="not-found"){
+      return (
+        <Modal className="wide">
+          <div className="ui top serverboards secondary menu">
+            <h3 className="ui header">{this.props.widget_id}</h3>
+            <div className="right menu">
+              <HoldButton className="item" onHoldClick={this.removeWidget}>{i18n("Remove")} <i className="ui icon trash"/></HoldButton>
+            </div>
+          </div>
+          <div className="ui text container">
+            <Error>
+              {i18n("Could not load information about this widget. Maybe the plugin was deleted?\n\nTry to install it again, or remove the widget.")}
+            </Error>
+          </div>
         </Modal>
       )
     }
--- frontend/app/js/containers/project/board/edit_widget.js
+++ frontend/app/js/containers/project/board/edit_widget.js
@@ -4,7 +4,10 @@ import View from 'app/components/project/board/edit_widget'
 const Controller = connect(
   (state, props) => ({
     widget_catalog: state.project.widget_catalog,
-    template: state.project.widget_catalog && state.project.widget_catalog.find( (w) => (w.id == props.widget.widget) )
+    template: state.project.widget_catalog && (
+      state.project.widget_catalog.find( (w) => (w.id == props.widget.widget) ) || "not-found"
+    ),
+    widget_id: props.widget.widget,
   }),
   (dispatch, prop) => ({}),
 )(View)
