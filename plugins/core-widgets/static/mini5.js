c9bc5a64ed3fe8cfce382b5ebcae2cc1caed8a53
David Moreno Montero <dmoreno@coralbits.com>
2018-01-05 14:18:51 +0100

    Added initial mini5 widget
    Show 5 items of information

---
+++ plugins/core-widgets/static/mini5.js
@@ -0,0 +1,42 @@
+(function(){
+  const React=Serverboards.React
+
+  class Mini5 extends React.Component{
+    componentDidMount(){
+      const props = this.props
+      props.setClass( `${props.config.color} card` )
+      props.setTitle(" ")
+    }
+    render(){
+      const props = this.props
+      const config = props.config
+      console.log("config is", config)
+
+      return React.createElement('div', {className: "ui content half padding"}, [
+        React.createElement('h3',{className: "ui white header text"}, [config.title]),
+        React.createElement('div',{className: "ui two column grid"}, [
+          React.createElement('div', {className: "column"}, [
+            React.createElement('div', {style:{ display: "flex", alignItems: "center", height: 25}}, [
+              React.createElement('i', {className: `${config.icon_left} icon`}, []),
+              React.createElement('span', {className: "ui biggier text"}, [ config.expr_left ])
+            ]),
+            React.createElement('div', {}, [
+              config.text_left
+            ])
+          ]),
+          React.createElement('div', {className: "column"}, [
+            React.createElement('div', {style:{ display: "flex", alignItems: "center", height: 25}}, [
+              React.createElement('i', {className: `${config.icon_right} icon`}, []),
+              React.createElement('span', {className: "ui biggier text"}, [ config.expr_right ])
+            ]),
+            React.createElement('div', {}, [
+              config.text_right
+            ])
+          ])
+        ])
+      ])
+    }
+  }
+
+  Serverboards.add_widget("serverboards.core.widgets/mini5", Mini5, {react: true})
+})()
