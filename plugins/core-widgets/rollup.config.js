bc3732fbc6d25e0c03c0351314751dc1aebfcf90
David Moreno Montero <dmoreno@coralbits.com>
2018-01-25 10:35:00 +0100

    Uglify core-widgets

--- plugins/core-widgets/rollup.config.js
+++ plugins/core-widgets/rollup.config.js
@@ -1,5 +1,6 @@
 import babel from 'rollup-plugin-babel';
 import nodeResolve from 'rollup-plugin-node-resolve';
+import uglify from 'rollup-plugin-uglify';
 
 const widgets = ["mini5", "clock", "mini2", "markdown"]
 
@@ -7,7 +8,8 @@ const configs = widgets.map( w => ({
     input: `src/${w}.js`,
     plugins: [
       babel(),
-      nodeResolve({})
+      nodeResolve({}),
+      uglify()
      ],
     output: {
       format: 'umd',
