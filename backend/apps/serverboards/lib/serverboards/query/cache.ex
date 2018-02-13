9fa0c30fded561a2d56b66edbfccae01257dd669
David Moreno <dmoreno@coralbits.com>
2018-02-13 17:29:25 +0100

    Perform value calculations on original process
    Returns a :youdoit to perform the calculation only when required.
    
    If there is a falure, it is converted to an error tuple, and returned
    (exceptions are "lost").

--- backend/apps/serverboards/lib/serverboards/query/cache.ex
+++ backend/apps/serverboards/lib/serverboards/query/cache.ex
@@ -22,6 +22,22 @@ defmodule Serverboards.Query.Cache do
   # Make the server ask for the data
   defp get_at_genserver(id, f, options) do
     val = GenServer.call(__MODULE__, {:get, id, f}, 60_000)
+    val = if val == :youdoit do # this marks I must do it, and then send the value for the rest.
+      val = try do
+        f.()
+      catch
+        :exit, _ ->
+          {:error, :exit}
+        any, any ->
+          {:error, {any, any}}
+      rescue
+        error ->
+          {:error, error}
+      end
+      GenServer.cast(__MODULE__, {:insert, id, val})
+      val
+    else val end
+
     case options[:ttl] do
       nil -> :ok
       ttl ->
@@ -49,15 +65,16 @@ defmodule Serverboards.Query.Cache do
         {:reply, value, status}
       _ ->
         # try get again, as several calls may have been queued
-        prev_from = Map.get(status,id, [])
-        if prev_from == [] do # the first calculates the value
-          Task.start_link(fn ->
-            value = f.()
-            GenServer.cast(__MODULE__, {:insert, id, value})
-          end)
+        prev_from = Map.get(status,id, nil)
+        if prev_from == nil do # the first calculates the value
+          status = Map.put(status, id, [] )
+          # Logger.debug("No answer, you do it #{inspect status}")
+          {:reply, :youdoit, status}
+        else
+          # Logger.debug("No anwser, wait for it #{inspect status}")
+          status = Map.put(status, id, [from | prev_from] )
+          {:noreply, status}
         end
-        status = Map.put(status, id, [from | prev_from] )
-        {:noreply, status}
     end
   end
 
@@ -68,6 +85,7 @@ defmodule Serverboards.Query.Cache do
   def handle_cast({:insert, id, value}, status) do
     :ets.insert(__MODULE__, {id, value})
 
+    # Logger.debug("Got answer for #{inspect id} -> #{inspect value}: #{inspect status[id]}")
     Enum.map(status[id], fn from ->
       GenServer.reply(from, value)
     end)
