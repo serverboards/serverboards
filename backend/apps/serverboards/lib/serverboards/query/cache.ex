b6e7e8c9c89610cebbaa5c5fed0a5de1544d9a31
David Moreno <dmoreno@coralbits.com>
2018-02-13 17:11:21 +0100

    Can perform several cache queries at the same time
    Cahce queries are no longer serialized, but create a task per query.

--- backend/apps/serverboards/lib/serverboards/query/cache.ex
+++ backend/apps/serverboards/lib/serverboards/query/cache.ex
@@ -10,42 +10,68 @@ defmodule Serverboards.Query.Cache do
   def get(id, f, options) do
     # Logger.debug("Get #{inspect id}")
     case :ets.lookup(__MODULE__, id) do
+      [{^id, :running}] ->
+        get_at_genserver(id, f, options)
       [{^id, value}] ->
         value
       other ->
-        val = GenServer.call(__MODULE__, {:insert, id, f}, 60_000)
-        case options[:ttl] do
-          nil -> :ok
-          ttl ->
-            Process.send_after(__MODULE__, {:remove, id}, ttl)
-        end
-        val
+        get_at_genserver(id, f, options)
     end
+  end
+
+  # Make the server ask for the data
+  defp get_at_genserver(id, f, options) do
+    val = GenServer.call(__MODULE__, {:get, id, f}, 60_000)
+    case options[:ttl] do
+      nil -> :ok
+      ttl ->
+        Process.send_after(__MODULE__, {:remove, id}, ttl)
+    end
+    val
   end
 
   ## impl
 
   def init(nil) do
     :ets.new(__MODULE__, [:named_table])
-    {:ok, :ok}
+
+    # status is a map of {id, f} to queue of requesters
+    {:ok, %{}}
   end
 
-  def handle_call({:insert, id, f}, _from, status) do
-    # try get again, as several calls may have been queued
-    val = case :ets.lookup(__MODULE__, id) do
+  def calculate_and_add(id, f) do
+    value = f.()
+  end
+
+  def handle_call({:get, id, f}, from, status) do
+    case :ets.lookup(__MODULE__, id) do
       [{^id, value}] ->
-        value
-      _other ->
-        # Logger.debug("Calculate value for #{inspect id}")
-        value = f.()
-        :ets.insert(__MODULE__, {id, value})
-        value
+        {:reply, value, status}
+      _ ->
+        # try get again, as several calls may have been queued
+        prev_from = Map.get(status,id, [])
+        if prev_from == [] do # the first calculates the value
+          Task.start_link(fn ->
+            value = f.()
+            GenServer.cast(__MODULE__, {:insert, id, value})
+          end)
+        end
+        status = Map.put(status, id, [from | prev_from] )
+        {:noreply, status}
     end
-    {:reply, val, status}
   end
 
   def handle_info({:remove, id}, status) do
     :ets.delete(__MODULE__, id)
+    {:noreply, status}
+  end
+  def handle_cast({:insert, id, value}, status) do
+    :ets.insert(__MODULE__, {id, value})
+
+    Enum.map(status[id], fn from ->
+      GenServer.reply(from, value)
+    end)
+    status = Map.drop(status, [id])
     {:noreply, status}
   end
 end
