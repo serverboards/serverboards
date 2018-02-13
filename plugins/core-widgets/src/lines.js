fea6fdeaee09bd77c12f342f4ae1829f32f4c485
David Moreno <dmoreno@coralbits.com>
2018-02-13 18:24:45 +0100

    Adde background lines to line graphs

--- plugins/core-widgets/src/lines.js
+++ plugins/core-widgets/src/lines.js
@@ -4,7 +4,14 @@ import GraphWithData from './graph_with_data'
 
 const svg_style = {
   axis_bottom: {
-    fill: "#000"
+    stroke: "#333",
+    strokeWidth: 1,
+    vectorEffect: "non-scalling-stroke"
+  },
+  axis_line: {
+    stroke: "#aaa",
+    strokeWidth: 0.5,
+    vectorEffect: "non-scalling-stroke"
   },
   axis: {
     fill: "#9b9b9b"
@@ -42,24 +49,37 @@ function SVGLines({data, xaxis, maxy, categories}){
     return ret
   }
 
+  const show_one_in = Math.ceil(xaxis.length / 10.0)
+  function show_category(i){
+    // console.log("Show %d? %d", i, (i % show_one_in) == 0)
+    return (i % show_one_in) == 0
+  }
+  // console.log("Show one cat in %d", show_one_in)
+
   // console.log(fill)
   return (
     <svg height={250} width={400}>
       <g>
-        <text x={30} y={25} textAnchor="end" fill={svg_style.grey}>{maxy}</text>
-        <text x={30} y={75} textAnchor="end" fill={svg_style.grey}>{maxy*3/4}</text>
-        <text x={30} y={125} textAnchor="end" fill={svg_style.grey}>{maxy*2/4}</text>
-        <text x={30} y={175} textAnchor="end" fill={svg_style.grey}>{maxy/4}</text>
-        <text x={30} y={225} textAnchor="end" fill={svg_style.grey}>0</text>
-        <line x1={40} y1={220} x2={390} y2={220} style={svg_style.axis_bottom}/>
-        {xaxis.map( (legend,i) => (
+        <text x={25} y={25} textAnchor="end" fill={svg_style.grey}>{maxy}</text>
+        <text x={25} y={75} textAnchor="end" fill={svg_style.grey}>{maxy*3/4}</text>
+        <text x={25} y={125} textAnchor="end" fill={svg_style.grey}>{maxy*2/4}</text>
+        <text x={25} y={175} textAnchor="end" fill={svg_style.grey}>{maxy/4}</text>
+        <text x={25} y={225} textAnchor="end" fill={svg_style.grey}>0</text>
+
+        <line x1={30} y1={20} x2={390} y2={20} style={svg_style.axis_line}/>
+        <line x1={30} y1={70} x2={390} y2={70} style={svg_style.axis_line}/>
+        <line x1={30} y1={120} x2={390} y2={120} style={svg_style.axis_line}/>
+        <line x1={30} y1={170} x2={390} y2={170} style={svg_style.axis_line}/>
+        <line x1={30} y1={220} x2={390} y2={220} style={svg_style.axis_bottom}/>
+
+        {xaxis.map( (legend,i) => show_category(i) && (
           <text key={i} x={xstart + i*xgap} y={235} style={svg_style.axis}>{legend}</text>
         ))}
       </g>
       <g>
-        {categories.map( (category,i) =>
+        {categories.map( (category,i) => (
           <polyline key={category} points={get_line(category)} style={{stroke: fill[i], fill: "none", strokeWidth: 2}}/>
-        ) }
+        ) ) }
       </g>
     </svg>
   )
