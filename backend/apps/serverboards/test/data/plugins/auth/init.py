c9b262dbcff4fc0ebf88cfdf958c2ab820eebd7f
David Moreno <dmoreno@coralbits.com>
2018-02-20 09:08:51 +0100

    Increased time for init test
    With the idea of having less extra log not really required.

--- backend/apps/serverboards/test/data/plugins/auth/init.py
+++ backend/apps/serverboards/test/data/plugins/auth/init.py
@@ -5,12 +5,12 @@ import serverboards, time
 @serverboards.rpc_method
 def init():
     """
-    Sleeps one second, wants to be restarted in one second
+    Sleeps one second, wants to be restarted in 30 seconds
     """
     serverboards.info("Init test running")
     time.sleep(0.5)
     serverboards.info("Init test stop")
-    return 2
+    return 30
 
 @serverboards.rpc_method
 def fail():
