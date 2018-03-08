d4dd353cbded501b86f4f28d78f25cdfc98c3c46
David Moreno Montero <dmoreno@coralbits.com>
2018-03-08 12:59:31 +0100

    Added y grid lines to bar graphs

--- plugins/core-widgets/src/bars.js
+++ plugins/core-widgets/src/bars.js
@@ -6,6 +6,12 @@ const svg_style = {
   axis_bottom: {
     fill: "#000"
   },
+  axis_line: {
+    stroke: "#aaa",
+    strokeWidth: 0.5,
+    vectorEffect: "non-scalling-stroke"
+  },
+
   axis: {
     fill: "#9b9b9b"
   },
@@ -36,11 +42,19 @@ function SVGBars({data, xaxis, maxy, categories}){
   return (
     <svg height={250} width={400}>
       <g>
-        <text x={30} y={25} textAnchor="end" fill={svg_style.grey}>{Math.round(maxy)}</text>
-        <text x={30} y={75} textAnchor="end" fill={svg_style.grey}>{Math.round(maxy*3/4)}</text>
-        <text x={30} y={125} textAnchor="end" fill={svg_style.grey}>{Math.round(maxy*2/4)}</text>
-        <text x={30} y={175} textAnchor="end" fill={svg_style.grey}>{Math.round(maxy/4)}</text>
-        <text x={30} y={225} textAnchor="end" fill={svg_style.grey}>0</text>
+        <text x={25} y={25} textAnchor="end" fill={svg_style.grey}>{maxy}</text>
+        <text x={25} y={75} textAnchor="end" fill={svg_style.grey}>{maxy*3/4}</text>
+        <text x={25} y={125} textAnchor="end" fill={svg_style.grey}>{maxy*2/4}</text>
+        <text x={25} y={175} textAnchor="end" fill={svg_style.grey}>{maxy/4}</text>
+        <text x={25} y={225} textAnchor="end" fill={svg_style.grey}>0</text>
+
+        <line x1={30} y1={20} x2={390} y2={20} style={svg_style.axis_line}/>
+        <line x1={30} y1={70} x2={390} y2={70} style={svg_style.axis_line}/>
+        <line x1={30} y1={120} x2={390} y2={120} style={svg_style.axis_line}/>
+        <line x1={30} y1={170} x2={390} y2={170} style={svg_style.axis_line}/>
+        <line x1={30} y1={220} x2={390} y2={220} style={svg_style.axis_bottom}/>
+
+
         <line x1={40} y1={220} x2={390} y2={220} style={svg_style.axis_bottom}/>
         {xaxis.map( (legend,i) => (
           <text key={i} x={xstart + i*xgap} y={235} style={svg_style.axis}>{legend}</text>
