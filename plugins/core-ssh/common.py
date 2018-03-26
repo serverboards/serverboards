ff1dae7d8e489dd743b0e141f879b7a9ea568258
David Moreno <dmoreno@coralbits.com>
2018-03-26 17:24:23 +0200

    PEP8

--- plugins/core-ssh/common.py
+++ plugins/core-ssh/common.py
@@ -1,20 +1,21 @@
 import os
 import serverboards
 
-ID_RSA=os.path.expanduser("~/id_rsa")
-CONFIG_FILE=os.path.expanduser("~/ssh_config")
-KNOWN_HOSTS_FILE=os.path.expanduser("~/known_hosts")
-ID_RSA_PUB=ID_RSA+'.pub'
+ID_RSA = os.path.expanduser("~/id_rsa")
+CONFIG_FILE = os.path.expanduser("~/ssh_config")
+KNOWN_HOSTS_FILE = os.path.expanduser("~/known_hosts")
+ID_RSA_PUB = ID_RSA + '.pub'
+
 
 def ensure_ID_RSA():
     if not os.path.exists(ID_RSA):
         serverboards.info("Generating new SSH key pair")
-        os.system('ssh-keygen -f "%s" -N "" >&2'%(ID_RSA,))
+        os.system('ssh-keygen -f "%s" -N "" >&2' % (ID_RSA,))
         os.chmod(ID_RSA, 0o0600)
     if not os.path.exists(CONFIG_FILE):
         serverboards.info("Creating the ssh config file")
-        with open(CONFIG_FILE,"w+") as fd:
-            fd.write("UserKnownHostsFile %s"%KNOWN_HOSTS_FILE)
+        with open(CONFIG_FILE, "w+") as fd:
+            fd.write("UserKnownHostsFile %s" % KNOWN_HOSTS_FILE)
             fd.write("\n\n")
             fd.write("# Write here your custom configuration\n")
         os.chmod(CONFIG_FILE, 0o0600)
--- plugins/core-ssh/serverboards-ssh-mgmt.py
+++ plugins/core-ssh/serverboards-ssh-mgmt.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-import sys, os, pty
-sys.path.append(os.path.join(os.path.dirname(__file__),'../bindings/python/'))
-import serverboards, pexpect, shlex, re, subprocess, random
+import sys
+import os
+import subprocess
 import urllib.parse as urlparse
-from common import *
+from common import ensure_ID_RSA, ID_RSA_PUB, KNOWN_HOSTS_FILE
+sys.path.append(os.path.join(os.path.dirname(__file__), '../bindings/python/'))
+import serverboards
 from serverboards import print
+
 
 @serverboards.rpc_method
 def ssh_public_key(**kwargs):
@@ -12,9 +15,10 @@ def ssh_public_key(**kwargs):
     with open(ID_RSA_PUB, 'r') as fd:
         return fd.read()
 
+
 def ssh_urlparse(url):
-    if not "://" in url:
-        url="ssh://%s"%(url)
+    if "://" not in url:
+        url = "ssh://%s" % (url)
     u = urlparse.urlparse(url)
     assert u.scheme == 'ssh'
     port = u.port or '22'
@@ -26,101 +30,124 @@ def get_fingerprint(url, *args, **kwargs):
         return None
     (hostname, port) = ssh_urlparse(url)
     try:
-        output = str(subprocess.check_output(["ssh-keyscan", "-p", port, hostname], timeout=5), 'utf8')
-        output=output.strip().split('\n')
+        output = str(subprocess.check_output(
+            ["ssh-keyscan", "-p", port, hostname], timeout=5), 'utf8')
+        output = output.strip().split('\n')
         output.sort()
         # serverboards.debug(repr(output))
         return '\n'.join(output)
     except subprocess.TimeoutExpired:
         return None
-    except:
-        import traceback; traceback.print_exc()
+    except Exception:
+        import traceback
+        traceback.print_exc()
         return None
+
 
 @serverboards.rpc_method
 def remote_fingerprint(url="", options="", **kwargs):
     # print(url, options, repr(kwargs))
-    fingerprint=get_fingerprint(url, options)
+    fingerprint = get_fingerprint(url, options)
     if not fingerprint:
         return {
-            "fingerprint": "Cant connect to server at <%s>. Set a valid SSH address."%url,
+            "fingerprint":
+                "Cant connect to server at <%s>. Set a valid SSH address." %
+                url,
             "className": "grey disabled",
             "toggle": "Select a valid SSH address",
             "enabled": False
         }
     fingerprintb = bytes(fingerprint, 'utf8')
 
-    sp=subprocess.Popen(["ssh-keygen", "-lf", "/dev/stdin", "-E", "md5"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    sp = subprocess.Popen(["ssh-keygen", "-lf", "/dev/stdin",
+                           "-E", "md5"], stdin=subprocess.PIPE,
+                          stdout=subprocess.PIPE)
     fingerprint_md5, error = sp.communicate(fingerprintb)
 
-    sp=subprocess.Popen(["ssh-keygen", "-lf", "/dev/stdin", "-E", "sha1"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    sp = subprocess.Popen(["ssh-keygen", "-lf", "/dev/stdin", "-E",
+                           "sha1"], stdin=subprocess.PIPE,
+                          stdout=subprocess.PIPE)
     fingerprint_sha1, error = sp.communicate(fingerprintb)
 
-    sp=subprocess.Popen(["ssh-keygen", "-lf", "/dev/stdin", "-E", "sha256"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    sp = subprocess.Popen(["ssh-keygen", "-lf", "/dev/stdin", "-E",
+                           "sha256"], stdin=subprocess.PIPE,
+                          stdout=subprocess.PIPE)
     fingerprint_sha256, error = sp.communicate(fingerprintb)
 
-    fingerprint_text = ''.join( [str(fingerprint_md5, 'utf8'), "\n", str(fingerprint_sha1, 'utf8'), "\n", str(fingerprint_sha256, 'utf8')] )
+    fingerprint_text = ''.join([str(fingerprint_md5, 'utf8'), "\n", str(
+        fingerprint_sha1, 'utf8'), "\n", str(fingerprint_sha256, 'utf8')])
 
-    enabled=False
+    enabled = False
     if os.path.exists(KNOWN_HOSTS_FILE):
-        with open(KNOWN_HOSTS_FILE,"r") as fd:
+        with open(KNOWN_HOSTS_FILE, "r") as fd:
             enabled = fingerprint in fd.read()
 
     return {
-        "fingerprint_orig" : fingerprint,
+        "fingerprint_orig": fingerprint,
         "fingerprint": fingerprint_text,
-        "toggle" : "Disable host" if enabled else "Enable host",
-        "className" : "red" if enabled else "yellow",
+        "toggle": "Disable host" if enabled else "Enable host",
+        "className": "red" if enabled else "yellow",
         "enabled": enabled
     }
 
+
 @serverboards.rpc_method
 def toggle_remote_fingerprint(url=None, status=None, options="", **args):
-    fingerprint=get_fingerprint(url, options)
+    fingerprint = get_fingerprint(url, options)
 
-    enabled=False
+    enabled = False
 
-    #serverboards.debug(repr(status["fingerprint_orig"]))
-    #serverboards.debug(repr(fingerprint))
-    #serverboards.debug(repr(status["fingerprint_orig"]==fingerprint))
+    # serverboards.debug(repr(status["fingerprint_orig"]))
+    # serverboards.debug(repr(fingerprint))
+    # serverboards.debug(repr(status["fingerprint_orig"]==fingerprint))
 
-    if status["fingerprint_orig"]==fingerprint:
+    if status["fingerprint_orig"] == fingerprint:
         if os.path.exists(KNOWN_HOSTS_FILE):
-            with open(KNOWN_HOSTS_FILE,"r") as fd:
+            with open(KNOWN_HOSTS_FILE, "r") as fd:
                 enabled = fingerprint in fd.read()
         if enabled:
-            hostname=fingerprint.split(' ')[0]
-            done=False
+            hostname = fingerprint.split(' ')[0]
+            done = False
             with open(KNOWN_HOSTS_FILE, "r") as rd:
-                with open(KNOWN_HOSTS_FILE+".bak", "w") as wd:
+                with open(KNOWN_HOSTS_FILE + ".bak", "w") as wd:
                     for l in rd.readlines():
-                        #serverboards.debug(repr((l[:10], hostname, l.startswith(hostname))))
+                        # serverboards.debug(repr((l[:10], hostname,
+                        #                    l.startswith(hostname))))
 
                         if not l.startswith(hostname):
                             wd.write(l)
                         else:
-                            done=True
-            os.rename(KNOWN_HOSTS_FILE+".bak", KNOWN_HOSTS_FILE)
-            serverboards.info("Removed SSH known_host fingerprint for url %s"%(url))
+                            done = True
+            os.rename(KNOWN_HOSTS_FILE + ".bak", KNOWN_HOSTS_FILE)
+            serverboards.info(
+                "Removed SSH known_host fingerprint for url %s" % (url))
             if done:
                 return "Fingerprint removed"
             else:
-                raise Exception("Could not remove fingerprint. Fingerprint not found. %s"%(hostname))
+                raise Exception(
+                    "Could not remove fingerprint. Fingerprint not found. %s" %
+                    (hostname))
         else:
             with open(KNOWN_HOSTS_FILE, "a") as fd:
                 fd.write(fingerprint)
                 fd.write('\n')
-            serverboards.info("Added SSH known_host fingerprint for url %s"%(url))
+            serverboards.info(
+                "Added SSH known_host fingerprint for url %s" % (url))
             return "Fingerprint added"
     print("Orig: ", status["fingerprint_orig"])
     print("Real: ", fingerprint)
     raise Exception("Fingerprint has changed")
 
-def test():
-    print(get_fingerprint("ssh://testurl","User dmoreno\nProxyCommand ssh 192.168.1.200 -W 10.0.3.92:22 -q"))
 
-if __name__=='__main__':
-    if len(sys.argv)>1 and sys.argv[1]=="test":
+def test():
+    print(get_fingerprint(
+        "ssh://testurl",
+        "User dmoreno\nProxyCommand ssh 192.168.1.200 -W 10.0.3.92:22 -q"
+    ))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and sys.argv[1] == "test":
         test()
 
     serverboards.loop()
