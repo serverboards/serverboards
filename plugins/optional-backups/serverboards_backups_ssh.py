825d808db589885f99def8d16a330b756844edd6
David Moreno <dmoreno@coralbits.com>
2018-03-27 18:50:56 +0200

    Async backup ssh

--- plugins/optional-backups/serverboards_backups_ssh.py
+++ plugins/optional-backups/serverboards_backups_ssh.py
@@ -5,8 +5,9 @@ import os
 import re
 import datetime
 sys.path.append(os.path.join(os.path.dirname(__file__), '../bindings/python/'))
-import serverboards
-from serverboards import Plugin, print
+import serverboards_aio as serverboards
+from serverboards_aio import Plugin, print
+from pcolor import printc
 
 TEMPLATE_RE = re.compile(r"{{([\w.]*)}}")
 
@@ -29,28 +30,31 @@ def render_template_with_dates(template):
 
 
 @serverboards.rpc_method
-def read_source(fifofile, config, context={}):
+async def read_source(fifofile, config, context={}):
     print("Read from %s to %s" % (config, fifofile))
-    with Plugin("serverboards.core.ssh/sshcmd") as ssh:
-        ssh.run(service=config["service"], command=[
-                "cat", config["path"]], outfile=fifofile, context=context)
-        size = int(ssh.run(
+    async with Plugin("serverboards.core.ssh/sshcmd") as ssh:
+        await ssh.run(
+            service=config["service"],
+            command=["cat", config["path"]],
+            outfile=fifofile,
+            context=context)
+        size = int((await ssh.run(
             service=config["service"],
             command=["du", "-sb", config["path"]],
-            context=context)["stdout"].split()[0])
-        serverboards.info("Done, read %s" % size, **context)
+            context=context))["stdout"].split()[0])
+        await serverboards.info("Done, read %s" % size, **context)
         return size
 
 
 @serverboards.rpc_method
-def read_source_tar(fifofile, config, context={}):
+async def read_source_tar(fifofile, config, context={}):
     print("Read from %s to %s" % (config, fifofile))
-    with Plugin("serverboards.core.ssh/sshcmd") as ssh:
-        ssh.run(
+    async with Plugin("serverboards.core.ssh/sshcmd") as ssh:
+        await ssh.run(
             service=config["service"],
             command=["tar", "cz", config["path"]],
             outfile=fifofile, context=context)
-        output = ssh.run(
+        output = await ssh.run(
             service=config["service"],
             command=["du", "-sb", config["path"]],
             context=context)
@@ -58,29 +62,51 @@ def read_source_tar(fifofile, config, context={}):
         if not stdout:
             raise Exception("file not exists")
         size = int(stdout.split()[0])
-        serverboards.info("Done, read %s" % size, **context)
+        await serverboards.info("Done, read %s" % size, **context)
         return size
 
 
 @serverboards.rpc_method
-def write_destination(fifofile, config, context={}):
+async def write_destination(fifofile, config, context={}):
     print("Write to %s from %s" % (config, fifofile))
-    with Plugin("serverboards.core.ssh/sshcmd") as ssh:
+    async with Plugin("serverboards.core.ssh/sshcmd") as ssh:
         copyto = config["path"]
         if '{{' in copyto:
             copyto = render_template_with_dates(copyto)
-        ssh.run(service=config["service"], command=[
-                "cat", ">", copyto], infile=fifofile, context=context)
-        stdout = ssh.run(service=config["service"], command=[
-                         "du", "-sb", copyto], context=context)["stdout"]
+        await ssh.run(
+            service=config["service"], command=["cat", ">", copyto],
+            infile=fifofile, context=context)
+        stdout = (await ssh.run(
+            service=config["service"], command=["du", "-sb", copyto],
+            context=context))["stdout"]
         if not stdout:
             raise Exception("file not created")
         size = int(stdout.split()[0])
-        serverboards.info("Done, write %s" % size, **context)
+        await serverboards.info("Done, write %s" % size, **context)
         return size
 
 
+async def test():
+    printc("TESTING")
+    try:
+        os.unlink("/tmp/fifofile")
+    except Exception:
+        pass
+    os.mkfifo("/tmp/fifofile")
+    await read_source_tar("/tmp/fifofile", {"service": "XXX", "path": "."})
+    await write_destination("/tmp/fifofile", {"service": "XXX", "path": "/tmp/testbackup.tgz"})
+
+    await read_source("/tmp/fifofile", {"service": "XXX", "path": "/etc/hostname"})
+
+    os.unlink("/tmp/fifofile")
+    pass
+
+
 if 'test' in sys.argv:
-    print(render_template_with_dates("/tmp/test-{{date}}.tgz"))
+    import yaml
+    printc("Test")
+    mock_data = yaml.load(open("mock.yaml"))
+    serverboards.test_mode(test, mock_data=mock_data)
+    printc("DONE")
 else:
     serverboards.loop()
