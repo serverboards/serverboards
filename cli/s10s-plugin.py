6f05d9acc62b24b51cde1995bf2e1a0dfcd7d3cc
David Moreno <dmoreno@coralbits.com>
2018-05-01 21:38:40 +0200

    CLI Can check if plugins require update

--- cli/s10s-plugin.py
+++ cli/s10s-plugin.py
@@ -4,6 +4,8 @@ import sys
 import os
 import yaml
 import json
+import subprocess
+import re
 
 __doc__ = """s10s plugin -- Plugin management
 Allows to install, uninstall and update plugins.
@@ -12,6 +14,7 @@ It uses git URLS to manage the state.
 
 Plugin management:
     s10s plugin list [what]   -- Lists all the plugins
+    s10s plugin check         -- Checks if there is something to update
     s10s plugin install <url> -- Installs a plugin by URL
     s10s plugin remove <path|plugin_id>  -- Removes a plugin
     s10s plugin update <path|plugin_id>  -- Updates a plugin
@@ -64,7 +67,6 @@ def all_plugins():
         for ppath in os.listdir(p):
             path = os.path.join(p, ppath)
             manifest = os.path.join(path, "manifest.yaml")
-            extra = os.path.join(path, ".extra.yaml")
             if os.path.exists(manifest):
                 try:
                     with open(manifest) as fd:
@@ -76,16 +78,55 @@ def all_plugins():
                         "status": "manifest-broken",
                         "message": str(e)
                     }
+                extra = os.path.join(path, ".extra.yaml")
                 if os.path.exists(extra):
                     with open(extra) as fd:
                         data.update(yaml.load(fd))
+
+                # TODO: Will work on other sources later
+                gitdir = os.path.join(path, ".git")
+                if os.path.exists(gitdir):
+                    data["source"] = "git"
+
                 data["path"] = path
                 yield data
 
 
+def check_upgrades():
+    """
+    Checks every installed plugin for new upgrades.
+    """
+    AHEAD_RE = re.compile(b".*\[behind (\d*)\].*")
+    ret = []
+    for pl in all_plugins():
+        if pl.get("source") != "git":
+            continue
+
+        os.chdir(pl["path"])
+        os.system("git fetch --quiet")
+        output = subprocess.run(
+            ["git", "status", "--porcelain", "--branch"],
+            stdout=subprocess.PIPE
+        )
+        m = AHEAD_RE.match(output.stdout)
+        if m:
+            extrafile = os.path.join(pl["path"], ".extra.yaml")
+            if os.path.exists(extrafile):
+                with open(extrafile) as rd:
+                    extra = yaml.load(rd)
+            else:
+                extra = {}
+            extra["outdated"] = m.groups(1)
+            with open(extrafile, "w") as wd:
+                yaml.dump(extra, wd)
+
+            ret.append({"id": pl.get("id"), "outdated": True})
+    output_data(ret)
+
+
 def list_(what=[]):
     if not what:
-        what = ["id", "status", "version"]
+        what = ["id", "status", "version", "upgradable"]
     res = []
     for pl in all_plugins():
         res.append({k: pl.get(k) for k in what})
@@ -109,6 +150,8 @@ def main(argv):
         sys.exit(0)
     if argv[0] == 'list':
         list_(argv[1:])
+    if argv[0] == 'check':
+        check_upgrades()
 
 
 if __name__ == "__main__":
