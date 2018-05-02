8862411f6402956f3999f6d5269822eb726c1f6b
David Moreno <dmoreno@coralbits.com>
2018-05-02 23:03:21 +0200

    Fix access to project settings

--- frontend/app/js/components/project/settings.js
+++ frontend/app/js/components/project/settings.js
@@ -4,25 +4,27 @@ import Loading from '../loading'
 import HoldButton from '../holdbutton'
 import i18n from 'app/utils/i18n'
 
+function SettingsMenu(props){
+  return (
+    <div className="right menu">
+      <HoldButton className="item" onHoldClick={props.onDelete}>
+        {i18n("Delete project")}
+        <i className="ui icon trash left padding" />
+      </HoldButton>
+    </div>
+  )
+}
+
+
 class Settings extends React.Component{
   componentDidMount(){
-    this.props.setSectionMenu(this.render_menu)
+    this.props.setSectionMenu(SettingsMenu, {onDelete: this.handleDelete.bind(this)})
   }
   handleUpdate( project ){
     this.props.onUpdate( this.props.project.shortname, project )
   }
   handleDelete(){
     this.props.onDelete( this.props.project.shortname )
-  }
-  render_menu(){
-    return (
-      <div className="right menu">
-        <HoldButton className="item" onHoldClick={this.handleDelete.bind(this)}>
-          {i18n("Delete project")}
-          <i className="ui icon trash left padding" />
-        </HoldButton>
-      </div>
-    )
   }
   render(){
     return (
