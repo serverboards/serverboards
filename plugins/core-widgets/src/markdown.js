51e0592865069afb61f05b2198922cd50941f394
David Moreno Montero <dmoreno@coralbits.com>
2018-05-07 12:08:46 +0200

    Improved markdown widget

--- plugins/core-widgets/src/markdown.js
+++ plugins/core-widgets/src/markdown.js
@@ -1,10 +1,39 @@
-const MarkdownPreview = Serverboards.Components.MarkdownPreview
+const {MarkdownPreview, Loading} = Serverboards.Components
 const React = Serverboards.React
 
-function MarkdownWrapper(props){
-  return (
-    <MarkdownPreview value={props.config.text} className="content"/>
-  )
+class MarkdownWrapper extends React.Component{
+  constructor(props){
+    super(props)
+  }
+  componentDidMount(){
+    this.updateTitle()
+  }
+  updateTitle(){
+    const text = this.props.config.text
+    if (!text)
+      return
+    let title = text.split('\n')[0]
+    if (title.startsWith("#"))
+      title = title.slice(1)
+    this.props.setTitle(title)
+  }
+  componentDidUpdate(prevProps, prevState, snapshot){
+    if (this.props.config.text != prevProps.config.text)
+      this.updateTitle()
+  }
+  render(){
+    let text = this.props.config.text
+    if (!text)
+      return (
+        <Loading.Widget/>
+      )
+    text = text.split('\n').slice(1)
+    return (
+      <div className="ui padding">
+        <MarkdownPreview value={text.join('\n')} className="content"/>
+      </div>
+    )
+  }
 }
 
 Serverboards.add_widget("serverboards.core.widgets/markdown", MarkdownWrapper, {react: true})
