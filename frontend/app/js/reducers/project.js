d64a3efdf2b439867ce5c277190b57fd86e1fb27
David Moreno Montero <dmoreno@coralbits.com>
2018-05-07 13:03:57 +0200

    Fix date range do not match (+1 day error)
    When clamping dates to start and end, must add the extra day.

--- frontend/app/js/reducers/project.js
+++ frontend/app/js/reducers/project.js
@@ -151,7 +151,7 @@ function project(state=default_state, action){
         daterange.start=moment(daterange.end).subtract(300, "seconds")
       }
       if (range_s > (2 * 24 * 60 * 60)){ // More than 2 days, use full days
-        daterange.start = moment(daterange.start).set({hour: 0, minute: 0, second: 0, millisecond: 0})
+        daterange.start = moment(daterange.start).set({hour: 0, minute: 0, second: 0, millisecond: 0}).add(1, "day")
         daterange.end = moment(daterange.end).set({hour: 23, minute: 59, second: 59, millisecond: 999})
       }
       // console.log(daterange)
