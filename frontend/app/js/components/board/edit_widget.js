53f3fc32f1d7a81bce1092afa204a95d8976d031
David Moreno <dmoreno@coralbits.com>
2018-05-11 19:12:50 +0200

    Improvements to edit widget screen

--- frontend/app/js/components/board/edit_widget.js
+++ frontend/app/js/components/board/edit_widget.js
@@ -130,29 +130,33 @@ class EditWidget extends React.Component{
     const wwidth = layout.w*140
     const wheight = (layout.h*163)
     layout.width = wwidth
-    layout.height = wheight
+    layout.height = wheight - 60
 
     return (
       <div className="ui expand two column grid grey background" style={{margin: 0}}>
-        <div className="ui column with scroll with padding">
-          <div className="ui board with scroll" style={{flexDirection: "column"}}>
-            <div className="ui cards" style={{margin: 0, padding: "1em", justifyContent: "center"}}>
-              <div className="ui card" style={{maxHeight: wheight, minHeight: wheight, maxWidth: wwidth, minWidth: wwidth }}>
-                <Widget
-                  key={widget.uuid}
-                  widget={widget.widget}
-                  config={state.postconfig}
-                  uuid={widget.uuid}
-                  project={this.props.project}
-                  layout={layout}
-                  />
+        <div className="ui column with padding">
+          <div className="ui vertical split" style={{maxHeight: "calc(100vh - 140px)", overflow: "hidden"}}>
+            <div className="ui board with scroll" style={{flexDirection: "column", flex: 1, maxHeight: "calc(50vh - 70px)", minHeight: "calc(50vh - 70px)"}}>
+              <div className="ui cards" style={{margin: 0, padding: "1em", justifyContent: "center", minWidth: wwidth+50, minHeight: 50}}>
+                <div className="ui card" style={{maxHeight: wheight, minHeight: wheight, maxWidth: wwidth, minWidth: wwidth }}>
+                  <Widget
+                    key={widget.uuid}
+                    widget={widget.widget}
+                    config={state.postconfig}
+                    uuid={widget.uuid}
+                    project={this.props.project}
+                    layout={layout}
+                    />
+                </div>
               </div>
             </div>
             {state.errors.map( e => (
               <SQLError key={e} error={e}/>
             ))}
             <hr className="ui separator"/>
-            <ExtractorsHelp extractors={state.extractors}/>
+            <div className="ui round pane white background scroll with padding" style={{flex: 1, maxHeight: "calc(50vh - 115px)", minHeight: "calc(50vh - 115px)"}}>
+              <ExtractorsHelp extractors={state.extractors}/>
+            </div>
           </div>
         </div>
         <div className="ui column">
