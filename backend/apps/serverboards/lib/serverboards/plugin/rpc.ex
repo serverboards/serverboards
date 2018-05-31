c01bb96f79ba48ad4fb09a53c534aceae232ccb0
David Moreno <dmoreno@coralbits.com>
2018-05-31 18:24:35 +0200

    Removed `UUID.method` plugin calling

--- backend/apps/serverboards/lib/serverboards/plugin/rpc.ex
+++ backend/apps/serverboards/lib/serverboards/plugin/rpc.ex
@@ -94,17 +94,6 @@ defmodule Serverboards.Plugin.RPC do
         Plugin.Runner.call id, method, []
     end, [required_perm: "plugin"]
 
-    RPC.MethodCaller.add_method method_caller, "plugin.alias", fn
-      [id, newalias], context ->
-        case Plugin.Runner.get(id) do
-          :not_found ->
-            {:error, :not_found}
-          %{ pid: pid } ->
-            RPC.Context.update context, :plugin_aliases, [{ newalias, pid }]
-            true
-          end
-    end, [required_perm: "plugin", context: true]
-
     RPC.MethodCaller.add_method method_caller, "plugin.is_running", fn
       [id], context ->
         if has_perm_for_plugin(context, :any) do
@@ -230,87 +219,6 @@ defmodule Serverboards.Plugin.RPC do
           |> Serverboards.Utils.clean_struct
     end
 
-    # Catches all [UUID].method calls and do it. This is what makes call plugin by uuid work.
-    RPC.MethodCaller.add_method_caller method_caller, &call_with_uuid(&1),
-      [required_perm: "plugin", name: :call_with_uuid]
-
-    # Catches all [alias].method calls and do it. Alias are stores into the context
-    RPC.MethodCaller.add_method_caller method_caller, &call_with_alias(&1),
-      [required_perm: "plugin", name: :call_with_alias]
-
     {:ok, method_caller}
   end
-
-
-  # Method caller function UUID.method.
-  def call_with_uuid(%MOM.RPC.Message{ method: method, params: params, context: _context}) do
-    #Logger.debug("Try to call #{inspect msg}")
-    if method == "dir" do
-      {:ok, []} # Do not return it as it can lead to show of opaque pointers. Use alias.
-    else
-      case Regex.run(~r/(^[-0-9a-f]{36})\.(.*)$/, method) do
-        [_, id, method] ->
-          res = Plugin.Runner.call id, method, params
-          #Logger.debug("UUID result #{inspect res}")
-          res
-        _ -> :nok
-      end
-    end
-  end
-
-  def call_with_alias(%MOM.RPC.Message{ method: method, params: params, context: context}) do
-    if method == "dir" do
-      {:ok, alias_dir(context)}
-    else
-      case Regex.run(~r/(^[^.]+)\.(.*)$/, method) do
-        [_, alias_, method] ->
-          aliases = RPC.Context.get context, :plugin_aliases, %{}
-          cmd = Map.get aliases, alias_
-          #Logger.debug("Call with alias #{inspect alias_}")
-          cond do
-            cmd == nil ->
-              #Logger.debug("Not alias")
-              :nok
-            Process.alive? cmd ->
-              ret=Serverboards.IO.Cmd.call cmd, method, params
-              Logger.debug("Alias result #{inspect ret}")
-              ret
-            true ->
-              # not running anymore, remove it
-              Logger.debug("Plugin not running anymore")
-              RPC.Context.update context, :plugin_aliases, [{ alias_, nil }]
-              :nok
-          end
-        _ -> :nok
-      end
-    end
-  end
-
-  # returns a list of all known methods that use alias
-  defp alias_dir(context) do
-    aliases = RPC.Context.get context, :plugin_aliases, %{}
-    ret = aliases
-      |> Enum.flat_map(fn {alias_, cmd} ->
-        try do
-          if Process.alive? cmd do
-            {:ok, res} = Serverboards.IO.Cmd.call cmd, "dir", []
-            res |> Enum.map(fn d ->
-                #Logger.debug("Got function #{alias_} . #{d}")
-                "#{alias_}.#{d}"
-              end)
-          else
-            # remove from aliases
-            RPC.Context.update context, :plugin_aliases, [{alias_, nil}]
-            []
-          end
-        rescue
-          e ->
-            Logger.error("Plugin with alias #{alias_} #{inspect cmd} does not implement dir. Fix it.\n#{inspect e}\n#{Exception.format_stacktrace}")
-            []
-        end
-      end)
-    #Logger.debug("Alias methods are: #{inspect ret}")
-    ret
-  end
-
 end
