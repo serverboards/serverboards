871a2c2f8024789a3152fd3bfd6cc90f4a754225
David Moreno <dmoreno@coralbits.com>
2018-07-18 12:59:12 +0200

    Added "POLL XX" support for USQ
    This allows to poll specific widgets at a given interval.

--- backend/apps/serverboards/lib/serverboards/query/query.ex
+++ backend/apps/serverboards/lib/serverboards/query/query.ex
@@ -30,60 +30,77 @@ defmodule Serverboards.Query do
     query = String.trim(query)
     # If not starts with SELECT, it is a simple text to use (rows at \n, cells at ,)
     if not is_sql(query) do
-      res = case query do
-        "" ->
-          %{ columns: ["?NONAME?"], rows: [[""]]}
-        _ ->
-          {:ok, stream} = StringIO.open(query)
-          stream = IO.binstream(stream, :line)
-          rows = CSV.decode(stream)
-            |> Enum.map(fn
-              {:ok, line} -> line
-              _other -> []
-            end)
-            |> Enum.filter(&(&1 != [])) # remove empty lines
-          columns = Enum.map(List.first(rows), fn _r -> "?NONAME?" end )
-          %{ columns: columns, rows: rows}
-      end
-      {:ok,  res}
+      csv_query(query)
     else
       # Logger.debug("Processed context #{inspect context}")
+      real_query(query, context)
+    end
+  end
 
-      try do
-        {time, result} = :timer.tc(ExoSQL, :query, [query, context])
-        with {:ok, %{ columns: columns, rows: rows}} <- result do
-          {:ok, %{ columns: columns, rows: rows, count: Enum.count(rows), time: time / 1_000_000.0}}
-        end
-      rescue
-        e in MatchError ->
-          Logger.error("Error performing query: #{inspect e}: #{inspect System.stacktrace(), pretty: true}")
-          # plan = with {:ok, parsed} <- ExoSQL.parse(query, context),
-          #             {:ok, plan} <- ExoSQL.plan(parsed, context) do
-          #               plan
-          #             end
-          # Logger.debug("Query plan is #{inspect plan, pretty: true}\n  #{inspect context["__vars__"], pretty: true}")
-          {:error, {:invalid_sql, e.term}}
-        exception in FunctionClauseError ->
-          Logger.error("#{inspect exception}: #{inspect System.stacktrace, pretty: true}")
-          {:error, :invalid_expression}
-        exception ->
-          Logger.error("#{inspect exception}: #{inspect System.stacktrace, pretty: true}")
-          {:error, "Meditation code: `#{inspect exception}`. Ask for help at the forums."}
-      catch
-        :exit, {:timeout, where} ->
-          Logger.error("Timeout performing query at #{inspect where, pretty: true}")
-          {:error, :timeout}
-        :exit, any ->
-          Logger.error("Error performing query: #{inspect any}: #{inspect System.stacktrace(), pretty: true}")
-          {:error, inspect(any)}
-        any ->
-          {:error, inspect(any)}
+  defp csv_query(query) do
+    res = case query do
+      "" ->
+        %{ columns: ["?NONAME?"], rows: [[""]]}
+      _ ->
+        {:ok, stream} = StringIO.open(query)
+        stream = IO.binstream(stream, :line)
+        rows = CSV.decode(stream)
+          |> Enum.map(fn
+            {:ok, line} -> line
+            _other -> []
+          end)
+          |> Enum.filter(&(&1 != [])) # remove empty lines
+        columns = Enum.map(List.first(rows), fn _r -> "?NONAME?" end )
+        %{ columns: columns, rows: rows}
+    end
+    {:ok,  res}
+  end
+
+
+  defp real_query("POLL " <> pquery, context) do
+    # Just ignore poll, this is a frontend issue. Its here becaus eits
+    # extrated from the widget config.
+    [_n, query] = String.split(String.trim(pquery), [" ", "\t", "\n"], parts: 2)
+    real_query(query, context)
+  end
+
+  defp real_query(query, context) do
+    try do
+      {time, result} = :timer.tc(ExoSQL, :query, [query, context])
+      with {:ok, %{ columns: columns, rows: rows}} <- result do
+        {:ok, %{ columns: columns, rows: rows, count: Enum.count(rows), time: time / 1_000_000.0}}
       end
+    rescue
+      e in MatchError ->
+        Logger.error("Error performing query: #{inspect e}: #{inspect System.stacktrace(), pretty: true}")
+        # plan = with {:ok, parsed} <- ExoSQL.parse(query, context),
+        #             {:ok, plan} <- ExoSQL.plan(parsed, context) do
+        #               plan
+        #             end
+        # Logger.debug("Query plan is #{inspect plan, pretty: true}\n  #{inspect context["__vars__"], pretty: true}")
+        {:error, {:invalid_sql, e.term}}
+      exception in FunctionClauseError ->
+        Logger.error("#{inspect exception}: #{inspect System.stacktrace, pretty: true}")
+        {:error, :invalid_expression}
+      exception ->
+        Logger.error("#{inspect exception}: #{inspect System.stacktrace, pretty: true}")
+        {:error, "Meditation code: `#{inspect exception}`. Ask for help at the forums."}
+    catch
+      :exit, {:timeout, where} ->
+        Logger.error("Timeout performing query at #{inspect where, pretty: true}")
+        {:error, :timeout}
+      :exit, any ->
+        Logger.error("Error performing query: #{inspect any}: #{inspect System.stacktrace(), pretty: true}")
+        {:error, inspect(any)}
+      any ->
+        {:error, inspect(any)}
     end
   end
 
   def is_sql(query) do
     query = String.upcase(query)
-    String.starts_with?(query, "SELECT") || String.starts_with?(query, "WITH")
+    String.starts_with?(query, "SELECT")
+      || String.starts_with?(query, "WITH")
+      || String.starts_with?(query, "POLL ")
   end
 end
