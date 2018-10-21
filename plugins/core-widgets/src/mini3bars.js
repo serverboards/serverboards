6dbdc0c7553aa51c74f692a47d47327a41133a04
David Moreno <dmoreno@coralbits.com>
2018-10-21 19:22:09 +0200

    Fix mini3bars title

--- plugins/core-widgets/src/mini3bars.js
+++ plugins/core-widgets/src/mini3bars.js
@@ -37,7 +37,7 @@ function prep_bars(bars){
 
 class Mini3Bars extends React.Component{
   componentDidMount(){
-    props.setTitle("|fullsize")
+    this.props.setTitle("|fullsize")
   }
   shouldComponentUpdate(nextprops){
     return !Serverboards.utils.object_is_equal( nextprops.config, this.props.config )
