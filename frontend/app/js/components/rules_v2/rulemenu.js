313700faa3c4f098094b35d5f01bb6bc82eeaa5a
David Moreno <dmoreno@coralbits.com>
2018-10-24 16:49:33 +0200

    Back button on rules

--- frontend/app/js/components/rules_v2/rulemenu.js
+++ frontend/app/js/components/rules_v2/rulemenu.js
@@ -3,14 +3,23 @@ import i18n from 'app/utils/i18n'
 
 function RuleMenu(props){
   return (
-    <div className="right menu">
+    <React.Fragment>
       <div className="item">
-        <a onClick={props.gotoRules} className="ui button basic">{props.cancel_label || i18n("Back to rules")}</a>
+      {props.cancel_label ? (
+        <a onClick={props.gotoRules} className="ui button basic">
+          {props.cancel_label}
+        </a>
+      ) : (
+        <a onClick={props.gotoRules} className="item">
+          <i className="ui chevron left icon"/> {i18n("Back")}
+        </a>
+      )}
       </div>
+      <div className="item stretch"/>
       <div className="item">
         <a onClick={props.saveRule} className={`ui button teal ${props.saveRule ? "" : "disabled"}`}>{props.save_label || i18n("Save rule")}</a>
       </div>
-    </div>
+    </React.Fragment>
   )
 }
 
