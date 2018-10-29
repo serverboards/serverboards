dc1eae518b16cce49b076ab81785ee8a7d530df8
David Moreno <dmoreno@coralbits.com>
2018-10-29 22:45:11 +0100

    WIP Use ACE editor instead of codemirror
    Codemirror react bindings were outdated, and ace looked promising as 
    simpler to
    embed and so on.

--- frontend/app/js/components/codemirror.js
+++ frontend/app/js/components/codemirror.js
@@ -1,50 +1,23 @@
 import React from 'react'
-import CodeMirror from 'react-codemirror'
-import CM from 'codemirror'
+import AceEditor from 'react-ace'
+import 'brace/ext/language_tools'
+import 'brace/mode/sql'
+import 'brace/theme/monokai'
 
-require('codemirror/mode/sql/sql')
-require('codemirror/mode/markdown/markdown')
-require('codemirror/lib/codemirror.css')
-require('codemirror/theme/solarized.css')
 
-function set(str) {
-  var obj = {}, words = str.split(" ");
-  for (var i = 0; i < words.length; ++i) obj[words[i]] = true;
-  return obj;
-}
-
-// Custom SQL dialect.
-CM.defineMIME("text/x-s10s", {
-  name: "sql",
-  client: set("source"),
-  keywords: set(
-    "select where from as inner cross left right outer join on" +
-    "group by order asc desc true false not distinct limit offset" +
-    "all null case if elif when then else end in like ilike"),
-  builtin: {},
-  atoms: set("false true null"),
-  operatorChars: /^[*+\-%<>!=&|^\/#@?~]/,
-  dateSQL: set("time"),
-  support: set("decimallessFloat zerolessFloat binaryNumber hexNumber")
-});
 
 function CodeMirrorWrapper(props){
   return (
-    <CodeMirror
+    <AceEditor
+      mode="sql"
+      theme="monokai"
+      fontSize={16}
+      width="100%"
+      height="10em"
+      style={{borderRadius: 3}}
+      enableBasicAutocompletion={true}
+      enableLiveAutocompletion={true}
       {...props}
-      options={{
-        theme:"solarized dark",
-        extraKeys: {
-          "Ctrl-Enter": props.onCtrlEnter,
-          "Ctrl-Space": "autocomplete"
-        },
-        hintOptions: props.hintOptions,
-        indentWithTabs: true,
-        smartIndent: true,
-        lineNumbers: true,
-        matchBrackets : true,
-        mode: props.mode == "sql" ? "text/x-s10s" : props.mode,
-      }}
       />
   )
 }
