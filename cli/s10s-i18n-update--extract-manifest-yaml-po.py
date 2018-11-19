18934be8fd7d8fa5932115a18856c1581cadd96a
David Moreno <dmoreno@coralbits.com>
2018-11-19 16:15:32 +0100

    PEP8

--- cli/s10s-i18n-update--extract-manifest-yaml-po.py
+++ cli/s10s-i18n-update--extract-manifest-yaml-po.py
@@ -1,30 +1,34 @@
 #!/usr/bin/python3
 
-import sys, yaml
+import sys
+import yaml
 
-EXTRACT_TAGS=["description", "name", "label"]
-FULL_EXTRACT_TAGS=["result"]
+EXTRACT_TAGS = ["description", "name", "label"]
+FULL_EXTRACT_TAGS = ["result"]
+known_texts = set()
+
 
 def cleanify(t):
     return t.replace("\n", "\\n").replace('"', '\\"')
 
-known_texts=set()
 
 def extract_text_in_context(k, context):
-    if context=="params":
-        return k!="name"
-    if context=="fields":
-        return k!="name"
+    if context == "params":
+        return k != "name"
+    if context == "fields":
+        return k != "name"
     return True
 
+
 def add_to_trans(v, filename):
-    v=cleanify(v)
-    if not v in known_texts:
-        print("#: %s"%filename)
-        print("msgid \"%s\""%(v))
+    v = cleanify(v)
+    if v not in known_texts:
+        print("#: %s" % filename)
+        print("msgid \"%s\"" % (v))
         print("msgstr \"\"")
         print("")
         known_texts.add(v)
+
 
 def full_extract(d, filename):
     if isinstance(d, str):
@@ -33,10 +37,12 @@ def full_extract(d, filename):
         for v in d.values():
             full_extract(v, filename)
 
+
 def extract_texts(d, context, filename):
     if isinstance(d, str):
         return
-    for k,v in d.items():
+
+    for k, v in d.items():
         if k in EXTRACT_TAGS:
             if extract_text_in_context(k, context):
                 add_to_trans(v, filename)
@@ -48,12 +54,13 @@ def extract_texts(d, context, filename):
             for i in v:
                 extract_texts(i, k, filename)
 
+
 for f in sys.argv[1:]:
     try:
         y = yaml.load(open(f))
         extract_texts(y, "root", f)
-    except:
-        print("Error parsing %s"%(f), file=sys.stderr)
+    except Exception:
+        print("Error parsing %s" % (f), file=sys.stderr)
         import traceback
         traceback.print_exc()
         print("", file=sys.stderr)
